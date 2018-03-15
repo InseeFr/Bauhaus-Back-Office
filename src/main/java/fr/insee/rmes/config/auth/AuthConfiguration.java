868691322646c7c723eeb5de9eefb06dc1652e1b
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-15 15:03:44 +0100

    Fix minor issue

--- src/main/java/fr/insee/rmes/config/auth/AuthConfiguration.java
+++ src/main/java/fr/insee/rmes/config/auth/AuthConfiguration.java
@@ -1,10 +1,5 @@
 package fr.insee.rmes.config.auth;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
