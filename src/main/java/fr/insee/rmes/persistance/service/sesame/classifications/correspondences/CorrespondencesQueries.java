a70f0a39377e1a1bb554b30ea9dd07ff0640b1b8
Nicolas Laval <nicolas.laval@insee.fr>
2018-06-04 17:19:52 +0200

    Fix minor issue

--- src/main/java/fr/insee/rmes/persistance/service/sesame/classifications/correspondences/CorrespondencesQueries.java
+++ src/main/java/fr/insee/rmes/persistance/service/sesame/classifications/correspondences/CorrespondencesQueries.java
@@ -20,8 +20,8 @@ public class CorrespondencesQueries {
 		String firstId = classificationsIds[0];
 		String secondId = classificationsIds[1];
 		return "SELECT ?id ?labelLg1 ?labelLg2 ?descriptionLg1 ?descriptionLg2 \n"
-				+ "?firstClasslabelLg1 ?firstClasslabelLg2 ?idFirstClass \n"
-				+ "?secondClasslabelLg1 ?secondClasslabelLg2 ?idSecondClass \n"
+				+ "?firstClassLabelLg1 ?firstClassLabelLg2 ?idFirstClass \n"
+				+ "?secondClassLabelLg1 ?secondClassLabelLg2 ?idSecondClass \n"
 				+ "WHERE { \n"
 				+ "?correspondence rdf:type xkos:Correspondence . \n"
 				+ "FILTER(REGEX(STR(?correspondence),'/codes/" + id + "')) . \n"
@@ -38,18 +38,18 @@ public class CorrespondencesQueries {
 				+ "?correspondence xkos:compares ?firstClassURI . \n"
 				+ "FILTER(REGEX(STR(?firstClassURI),'/codes/" + firstId + "/')) . \n"
 				+ "BIND('" + firstId + "' as ?idFirstClass) . \n"
-				+ "?firstClassURI skos:prefLabel ?firstClasslabelLg1 . \n"
-				+ "FILTER (lang(?firstClasslabelLg1) = '" + Config.LG1 + "')  . \n"
-				+ "OPTIONAL {?firstClassURI skos:prefLabel ?firstClasslabelLg2 . \n"
-				+ "FILTER (lang(?firstClasslabelLg2) = '" + Config.LG2 + "') }  . \n"
+				+ "?firstClassURI skos:prefLabel ?firstClassLabelLg1 . \n"
+				+ "FILTER (lang(?firstClassLabelLg1) = '" + Config.LG1 + "')  . \n"
+				+ "OPTIONAL {?firstClassURI skos:prefLabel ?firstClassLabelLg2 . \n"
+				+ "FILTER (lang(?firstClassLabelLg2) = '" + Config.LG2 + "') }  . \n"
 				// Second classification
 				+ "?correspondence xkos:compares ?secondClassURI . \n"
 				+ "FILTER(REGEX(STR(?secondClassURI),'/codes/" + secondId + "/')) . \n"
 				+ "BIND('" + secondId + "' as ?idSecondClass) . \n"
-				+ "?secondClassURI skos:prefLabel ?secondClasslabelLg1 . \n"
-				+ "FILTER (lang(?secondClasslabelLg1) = '" + Config.LG1 + "')  . \n"
-				+ "OPTIONAL {?secondClassURI skos:prefLabel ?secondClasslabelLg2 . \n"
-				+ "FILTER (lang(?secondClasslabelLg2) = '" + Config.LG2 + "') }  . \n"
+				+ "?secondClassURI skos:prefLabel ?secondClassLabelLg1 . \n"
+				+ "FILTER (lang(?secondClassLabelLg1) = '" + Config.LG1 + "')  . \n"
+				+ "OPTIONAL {?secondClassURI skos:prefLabel ?secondClassLabelLg2 . \n"
+				+ "FILTER (lang(?secondClassLabelLg2) = '" + Config.LG2 + "') }  . \n"
 				+ "}";
 	}
 	
