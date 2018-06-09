aafd40f24e1dfef513d5a36082adf952fdd51b51
Nicolas Laval <nicolas.laval@insee.fr>
2018-06-09 11:44:54 +0200

    Fix issue with public resource returns

--- src/main/java/fr/insee/rmes/webservice/PublicResources.java
+++ src/main/java/fr/insee/rmes/webservice/PublicResources.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
@@ -146,14 +147,16 @@ public class PublicResources {
 	@POST
 	@Path("/private/role/add")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void setAddRole(String body) {
+	public Response setAddRole(String body) {
 		userRolesManagerService.setAddRole(body);
+		return Response.status(Status.NO_CONTENT).build();
 	}
 
 	@POST
 	@Path("/private/role/delete")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void setDeleteRole(String body) {
+	public Response setDeleteRole(String body) {
 		userRolesManagerService.setDeleteRole(body);
+		return Response.status(Status.NO_CONTENT).build();
 	}
 }
