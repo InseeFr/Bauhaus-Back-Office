e64dee39eed9809d17ef1d7cdd1211c9db5fb4b1
Alice Lambois <alice.lambois@insee.fr>
2018-10-18 10:20:16 +0200

    Add freemarker to put queries in text files

--- src/main/java/fr/insee/rmes/persistance/service/sesame/operations/documentations/DocumentationsQueries.java
+++ src/main/java/fr/insee/rmes/persistance/service/sesame/operations/documentations/DocumentationsQueries.java
@@ -1,187 +1,122 @@
 package fr.insee.rmes.persistance.service.sesame.operations.documentations;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fr.insee.rmes.config.Config;
+import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.persistance.service.sesame.utils.FreeMarkerUtils;
 
 public class DocumentationsQueries {
-
-	public static String msdQuery() {
-		return "SELECT ?idMas ?masLabelLg1 ?masLabelLg2 ?idParent ?isPresentational \n"
-		 + " FROM <http://rdf.insee.fr/graphes/def/simsv2fr> \n"
-		+ " WHERE { \n"
-	    + " ?reportStructure sdmx-mm:metadataAttributeSpecification ?mas . \n"
-		+ "    ?mas rdfs:label ?masLabelLg1 ; \n" 
-		+ "      	rdfs:label ?masLabelLg2 ; \n" 
-		+ "    FILTER(lang(?masLabelLg1) = '" + Config.LG1 + "') \n" 
-		+ "    FILTER(lang(?masLabelLg2) = '" + Config.LG2 + "') \n" 
-		
-		+ "   OPTIONAL {?mas sdmx-mm:parent ?parent } \n"
-		+ "BIND(REPLACE( STR(?parent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . \n"
-
-		+ "   OPTIONAL {?mas sdmx-mm:isPresentational ?isPresentational } \n"
-		+ "FILTER(STRENDS(STR(?reportStructure),'/qualite/simsv2fr/reportStructure')) . \n"
-		
-		+ "BIND(REPLACE( STR(?mas) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idMas) . \n"
-
-
-		+ "  } \n"
-		+ "  ORDER BY ?mas";
+	
+	static Map<String,Object> params ;
+	
+	/**
+	 * @return ?idMas ?masLabelLg1 ?masLabelLg2 ?idParent ?isPresentational 
+	 * @throws RmesException
+	 */
+	public static String msdQuery() throws RmesException{
+		if (params==null) {initParams();}
+        return buildRequest("msdQuery.ftlh", params);
 	}
 	
-	public static String getAttributeSpecificationQuery(String idMas) {
-		return "SELECT ?masLabelLg1 ?masLabelLg2 ?range ?isPresentational \n" 
-				+ "  FROM <http://rdf.insee.fr/graphes/def/simsv2fr> \n" 
-				+ "  FROM <http://rdf.insee.fr/graphes/codes> \n" 
-				+ "  WHERE { \n" 
-				+ "    ?mas rdfs:label ?masLabelLg1 ; \n" 
-				+ "      	rdfs:label ?masLabelLg2 ; \n" 
-				+ "    FILTER(lang(?masLabelLg1) = '" + Config.LG1 + "') \n" 
-				+ "    FILTER(lang(?masLabelLg2) = '" + Config.LG2 + "') \n" 
-				+ "    OPTIONAL {?mas sdmx-mm:isPresentational ?isPresentational } \n" 
-								
-				+ "    ?mas sdmx-mm:metadataAttributeProperty ?map . \n"
-				+ "    OPTIONAL {?map rdfs:range ?range } \n" 
-								
-				+ "    FILTER(STRENDS(STR(?mas), '"+idMas+"')) \n"
-
-				+ "  } \n" 
-				+ "  ORDER BY ?mas";
+	/**
+	 * @param idMas
+	 * @return ?masLabelLg1 ?masLabelLg2 ?range ?isPresentational 
+	 * @throws RmesException
+	 */
+	public static String getAttributeSpecificationQuery(String idMas) throws RmesException {
+		if (params==null) {initParams();}
+		params.put("idMas", idMas);
+        return buildRequest("getAttributeSpecificationQuery.ftlh", params);
 	}
 	
-	public static String getAttributesQuery() {
-		return "SELECT ?id ?masLabelLg1 ?masLabelLg2 ?range ?isPresentational \n" 
-				+ "  FROM <http://rdf.insee.fr/graphes/def/simsv2fr> \n" 
-				+ "  FROM <http://rdf.insee.fr/graphes/codes> \n" 
-				+ "  WHERE { \n" 
-				+ "    ?mas rdfs:label ?masLabelLg1 ; \n" 
-				+ "      	rdfs:label ?masLabelLg2 ; \n" 
-				+ "    FILTER(lang(?masLabelLg1) = '" + Config.LG1 + "') \n" 
-				+ "    FILTER(lang(?masLabelLg2) = '" + Config.LG2 + "') \n" 
-				+ "    OPTIONAL {?mas sdmx-mm:isPresentational ?isPresentational } \n" 
-								
-				+ "    ?mas sdmx-mm:metadataAttributeProperty ?map . \n"
-				+ "    OPTIONAL {?map rdfs:range ?range } \n" 
-								
-				+ "BIND(REPLACE( STR(?mas) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . \n"
-
-				+ "  } \n" 
-				+ "  ORDER BY ?mas";
+	/**
+	 * @return ?id ?masLabelLg1 ?masLabelLg2 ?range ?isPresentational
+	 * @throws RmesException
+	 */
+	public static String getAttributesQuery() throws RmesException {
+		if (params==null) {initParams();}
+        return buildRequest("getAttributesQuery.ftlh", params);
 	}
 	
-	public static String getAttributesUriQuery() {
-		return "SELECT ?id ?uri \n" 
-				+ "  FROM <http://rdf.insee.fr/graphes/def/simsv2fr> \n" 
-				+ "  WHERE { \n" 
-				+ "    ?uri rdf:type sdmx-mm:MetadataAttributeProperty \n" 
-																				
-				+ "BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . \n"
-
-				+ "  } \n" 
-				+ "  ORDER BY ?id";
+	/**
+	 * @return ?id ?uri 
+	 * @throws RmesException
+	 */
+	public static String getAttributesUriQuery() throws RmesException {
+        return buildRequest("getAttributesUriQuery.ftlh", null);
 	}
 	
 	
-	public static String getDocumentationTitleQuery(String idSims) {
-		return "SELECT ?labelLg1 ?labelLg2 ?idOperation \n"
-				+ "FROM <http://rdf.insee.fr/graphes/qualite/rapport/" + idSims +"> \n"
-				+ " WHERE { \n"
-				+ " ?report rdf:type sdmx-mm:MetadataReport . \n"
-				
-				+ "OPTIONAL{ ?report sdmx-mm:target ?operation . \n"
-				+ " BIND(STRAFTER(STR(?operation),'/operation/') AS ?idOperation) . }\n" 
-				
-				+ " ?report rdfs:label ?labelLg1 . "
-				+ "    FILTER(lang(?labelLg1) = '" + Config.LG1 + "') \n" 
-				+ " ?report rdfs:label ?labelLg2 . "
-				+ "    FILTER(lang(?labelLg2) = '" + Config.LG2 + "') \n" 
-				
-				+ "    FILTER(STRENDS(STR(?report), '"+idSims+"')) \n"
-				+ "}";
-		
-
+	/**
+	 * @param idSims
+	 * @return ?labelLg1 ?labelLg2 ?idOperation
+	 * @throws RmesException
+	 */
+	public static String getDocumentationTitleQuery(String idSims) throws RmesException {
+		if (params==null) {initParams();}
+		params.put("idSims", idSims);
+        return buildRequest("getDocumentationTitleQuery.ftlh", params);
 	}
+	
 	/**
 	 * Get operation by documentation
+	 * @return ?idOperation 
+	 * @throws RmesException
 	 */
-	public static String getDocumentationOperationQuery(String idSims) {
-		return "SELECT ?idOperation \n"
-				+ "FROM <http://rdf.insee.fr/graphes/qualite/rapport/" + idSims +"> \n"
-				+ " WHERE { \n"
-				+ " ?report rdf:type sdmx-mm:MetadataReport . \n"
-				+ " ?report sdmx-mm:target ?operation . \n"
-				+ "BIND(STRAFTER(STR(?operation),'/operation/') AS ?idOperation) . \n" 
-				+ "}";
+	public static String getDocumentationOperationQuery(String idSims) throws RmesException {
+		Map<String, Object> root = new HashMap<>();
+		root.put("idSims", idSims);
+		return buildRequest("getDocumentationOperationQuery.ftlh", root);
+		
 	}
-	
+
 	/**
 	 * Get documentation by operation
+	 * @return ?idSims 
+	 * @throws RmesException 
 	 */
-	public static String getOperationDocumentationQuery(String idOperation) {
-		return "SELECT ?idSims \n"
-				+ " WHERE { \n"
-				+ " ?report rdf:type sdmx-mm:MetadataReport ."
-				+ " ?report sdmx-mm:target ?operation "
-				+ "    FILTER(STRENDS(STR(?operation), '"+idOperation+"')) \n"
-				+ " BIND(STRAFTER(STR(?report),'/rapport/') AS ?idSims) . \n" 
-				+ "}";
+	public static String getOperationDocumentationQuery(String idOperation) throws RmesException {
+		Map<String, Object> root = new HashMap<>();
+		root.put("idOperation", idOperation);
+		return buildRequest("getOperationDocumentationQuery.ftlh", root);
 	}
 	
-	public static String getDocumentationRubricsQuery(String idSims) {
-		//TODO RangeType.STRING, RangeType.ORGANIZATION, RangeType.UNDEFINED
-		return "SELECT ?idAttribute ?value ?labelLg1 ?labelLg2 ?codeList ?rangeType \n "
-			+ "FROM <http://rdf.insee.fr/graphes/qualite/rapport/" + idSims+"> \n " 
-			+" FROM <http://rdf.insee.fr/graphes/codes> \n"
-			+ "WHERE { \n "
-	
-			//RangeType.DATE --> value
-			+ "{\n"
-			+ " ?report rdf:type sdmx-mm:MetadataReport .\n "
-			+ " BIND(REPLACE( STR(?attr) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idAttribute) . \n"
-			+ " ?report ?attr ?value . "
-			+ " FILTER ( datatype(?value) = <"+RangeType.DATE.getRdfType()+"> ) "
-			+ "BIND('"+RangeType.DATE.getJsonType()+"' AS ?rangeType) . \n"
-
-			+"  } \n"
-			
-			 
-			//RangeType.ATTRIBUTE --> label
-			+ " UNION {\n"
-			+ " ?report rdf:type sdmx-mm:MetadataReport .\n "
-			+ " BIND(REPLACE( STR(?attr) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idAttribute) . \n"
-			+ " ?report ?attr ?node . "
-			+ " ?node rdf:value ?labelLg1 ."
-			+ "    FILTER(lang(?labelLg1) = '" + Config.LG1 + "') \n" 
-			+ " OPTIONAL{?node rdf:value ?labelLg2 ."
-			+ "    FILTER(lang(?labelLg2) = '" + Config.LG2 + "') } \n" 
-			+ "BIND('"+RangeType.ATTRIBUTE.getJsonType()+"' AS ?rangeType) . \n"
-
-			+"  } \n"
-			
-			//RangeType.CODELIST --> value
-			+ " UNION {\n"
-			+ " ?report rdf:type sdmx-mm:MetadataReport .\n "
-			+ " BIND(REPLACE( STR(?attr) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idAttribute) . \n"
-			+ " ?report ?attr ?codeUri . "
-			+ " ?codeUri skos:notation ?value ."
-			+ " ?codeUri skos:inScheme ?listUri ."
-			+ " ?listUri skos:notation ?codeList ."
-			+ "BIND('"+RangeType.CODELIST.getJsonType()+"' AS ?rangeType) . \n"
-
-			+"  } \n"
-			
-			+"}";
-		
+	/**
+	 * @param idSims
+	 * @return ?idAttribute ?value ?labelLg1 ?labelLg2 ?codeList ?rangeType 
+	 * @throws RmesException
+	 */
+	public static String getDocumentationRubricsQuery(String idSims) throws RmesException {
+		if (params==null) {initParams();}
+		params.put("idSims", idSims);
+		params.put("DATE", RangeType.DATE);
+		params.put("ATTRIBUTE", RangeType.ATTRIBUTE);
+		params.put("CODELIST", RangeType.CODELIST);
+		return buildRequest("getDocumentationRubricsQuery.ftlh", params);	
 	}
 	
-	public static String lastID() {
-		return "SELECT DISTINCT ?idSims \n" 
-				+ " WHERE {" 
-				+ "    GRAPH ?g {?report a sdmx-mm:MetadataReport } \n" 
-				+ "    BIND(STRAFTER(STR(?report),'/rapport/') AS ?idSims) . \n"
-				+ " }    \n"  
-				+ " GROUP BY ?g ?idSims \n"  
-				+ " ORDER BY DESC(?idSims)"
-				+ " LIMIT 1 ";
+	/**
+	 * @return ?idSims 
+	 * @throws RmesException
+	 */
+	public static String lastID() throws RmesException {
+        return buildRequest("lastID.ftlh", null);
 	}	
+	
+
+	private static void initParams() {
+		params = new HashMap<>();
+		params.put("LG1", Config.LG1);
+		params.put("LG2", Config.LG2);
+	}
+	
+	
+	private static String buildRequest(String fileName, Map<String, Object> params) throws RmesException  {
+		return FreeMarkerUtils.buildRequest("operations/documentations/", fileName, params);
+	}
+	
 	
 	 private DocumentationsQueries() {
 		 throw new IllegalStateException("Utility class");
