0dc315ad12a5fe530e570c1029ab8245ae5498e5
Nicolas Laval <nicolas.laval@insee.fr>
2019-08-26 14:35:20 +0200

    Add DSDs services

---
+++ src/main/java/fr/insee/rmes/config/swagger/model/dsd/IdLabelType.java
@@ -0,0 +1,16 @@
+package fr.insee.rmes.config.swagger.model.dsd;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class IdLabelType {
+	
+	@Schema(description = "Id", required = true)
+	public String id;
+	
+	@Schema(description = "Label", required = true)
+	public String label;
+	
+	@Schema(description = "Type", required = true)
+	public String type;
+
+}
