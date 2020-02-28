b4fc7e2489d89c87c85be03239878ad3b0a5f4da
Olivier Pucher <olivier.pucher@insee.fr>
2020-02-28 15:27:22 +0100

    include empty altLabels in operations list

--- src/main/java/fr/insee/rmes/persistance/service/sesame/operations/operations/OperationsQueries.java
+++ src/main/java/fr/insee/rmes/persistance/service/sesame/operations/operations/OperationsQueries.java
@@ -5,16 +5,16 @@ import fr.insee.rmes.config.Config;
 public class OperationsQueries {
 
 	public static String operationsQuery() {
-		return "SELECT DISTINCT ?id ?label ?altLabel \n"
+		return "SELECT DISTINCT ?id ?label (group_concat(?altLabelLg1;separator=' || ') as ?altLabel) \n"
 				+ "WHERE { GRAPH <http://rdf.insee.fr/graphes/operations> { \n"
 				+ "?operation a insee:StatisticalOperation . \n" 
 				+ "?operation skos:prefLabel ?label . \n"
 				+ "FILTER (lang(?label) = '" + Config.LG1 + "') \n"
 				+ "BIND(STRAFTER(STR(?operation),'/operations/operation/') AS ?id) . \n"
-				+ "OPTIONAL{?operation skos:altLabel ?altLabel . "
-				+ "FILTER (lang(?altLabel) = '" + Config.LG1 + "')}\n" 
+				+ "OPTIONAL{?operation skos:altLabel ?altLabelLg1 . "
+				+ "FILTER (lang(?altLabelLg1) = '" + Config.LG1 + "')}\n" 
 				+ "}} \n" 
-				+ "GROUP BY ?id ?label ?altLabel \n"
+				+ "GROUP BY ?id ?label \n"
 				+ "ORDER BY ?label ";
 	}
 
