a87e471b5eb4491f3e2b95aed47e1cec18581fc4
Olivier Pucher <olivier.pucher@insee.fr>
2020-09-29 16:59:47 +0200

    Reorganize Series Queries (part 2)

--- src/main/java/fr/insee/rmes/persistance/sparql_queries/operations/series/SeriesQueries.java
+++ src/main/java/fr/insee/rmes/persistance/sparql_queries/operations/series/SeriesQueries.java
@@ -18,28 +18,13 @@ public class SeriesQueries {
 	private static StringBuilder variables;
 	private static StringBuilder whereClause;
 	static Map<String, Object> params;
-
-	public static String seriesQuery() {
-		return "SELECT DISTINCT ?id ?label (group_concat(?altLabelLg1;separator=' || ') as ?altLabel) \n"
-				+ "WHERE { GRAPH <"+Config.OPERATIONS_GRAPH+"> { \n"
-				+ "?series a insee:StatisticalOperationSeries . \n" + "?series skos:prefLabel ?label . \n"
-				+ "FILTER (lang(?label) = '" + Config.LG1 + "') \n"
-				+ "BIND(STRAFTER(STR(?series),'/operations/serie/') AS ?id) . \n"
-				+ "OPTIONAL{?series skos:altLabel ?altLabelLg1 . " + "FILTER (lang(?altLabelLg1) = '" + Config.LG1
-				+ "') } \n " + "}} \n" + "GROUP BY ?id ?label \n" + "ORDER BY ?label ";
-	}
-
-	public static String seriesWithSimsQuery() {
-		return "SELECT DISTINCT ?id ?label ?idSims (group_concat(?altLabelLg1;separator=' || ') as ?altLabel) \n"
-				+ "WHERE { \n" + "GRAPH <"+Config.OPERATIONS_GRAPH+"> { \n"
-				+ "?series a insee:StatisticalOperationSeries . \n" + "?series skos:prefLabel ?label . \n"
-				+ "FILTER (lang(?label) = '" + Config.LG1 + "') \n"
-				+ "BIND(STRAFTER(STR(?series),'/operations/serie/') AS ?id) . \n"
-				+ "OPTIONAL{ ?report rdf:type sdmx-mm:MetadataReport ." + " ?report sdmx-mm:target ?series "
-				+ " BIND(STRAFTER(STR(?report),'/rapport/') AS ?idSims) . \n" + "} \n"
-				+ "OPTIONAL{?series skos:altLabel ?altLabelLg1 . " + "FILTER (lang(?altLabelLg1) = '" + Config.LG1
-				+ "') } \n " + "}" + "} \n" + "GROUP BY ?id ?label ?idSims \n" + "ORDER BY ?label ";
-	}
+	
+	private static final String ID_SERIES = "ID_SERIES";
+	private static final String URI_SERIES = "URI_SERIES";
+	private static final String ORGANIZATIONS_GRAPH = "ORGANIZATIONS_GRAPH";
+	private static final String OPERATIONS_GRAPH = "OPERATIONS_GRAPH";
+	private static final String ORG_INSEE_GRAPH = "ORG_INSEE_GRAPH";
+	private static final String LINK_PREDICATE = "LINK_PREDICATE";
 
 	public static String oneSeriesQuery(String id) {
 		variables = null;
@@ -60,111 +45,6 @@ public class SeriesQueries {
 		return "SELECT DISTINCT " + variables.toString() + " WHERE {  \n" + whereClause.toString() + "} \n";
 	}
 
-	public static String seriesLinks(String idSeries, IRI linkPredicate) {
-		return "SELECT ?id ?typeOfObject ?labelLg1 ?labelLg2 \n" + "WHERE { \n" + "?series <" + linkPredicate
-				+ "> ?uriLinked . \n" + "?uriLinked skos:prefLabel ?labelLg1 . \n" 
-				+ "FILTER (lang(?labelLg1) = '"+ Config.LG1 + "') . \n" 
-				+ "OPTIONAL {?uriLinked skos:prefLabel ?labelLg2 . \n"
-				+ "FILTER (lang(?labelLg2) = '" + Config.LG2 + "')} . \n" + "?uriLinked rdf:type ?typeOfObject . \n"
-				+ "BIND(REPLACE( STR(?uriLinked) , '(.*/)(\\\\w+$)', '$2' ) AS ?id) . \n"
-
-				+ "FILTER(STRENDS(STR(?series),'/operations/serie/" + idSeries + "')) . \n"
-
-				+ "} \n" + "ORDER BY ?labelLg1";
-	}
-
-	public static String getOperations(String idSeries) {
-		return "SELECT ?id ?labelLg1 ?labelLg2 \n" 
-				+ " FROM <"+Config.OPERATIONS_GRAPH+"> \n" 
-				+ "WHERE { \n"
-				+ "?series dcterms:hasPart ?uri . \n" + "?uri skos:prefLabel ?labelLg1 . \n"
-				+ "FILTER (lang(?labelLg1) = '" + Config.LG1 + "') . \n" + "?uri skos:prefLabel ?labelLg2 . \n"
-				+ "FILTER (lang(?labelLg2) = '" + Config.LG2 + "') . \n"
-				+ "BIND(STRAFTER(STR(?uri),'/operations/operation/') AS ?id) . \n"
-
-				+ "FILTER(STRENDS(STR(?series),'/operations/serie/" + idSeries + "')) . \n" + "}" + " ORDER BY ?id";
-	}
-
-	public static String getGeneratedWithOld(String idSeries) {
-		return "SELECT ?id ?typeOfObject ?labelLg1 ?labelLg2 \n" 
-				+ "WHERE { \n"
-
-				+ "?uri prov:wasGeneratedBy ?series . \n" + "?uri skos:prefLabel ?labelLg1 . \n"
-				+ "FILTER (lang(?labelLg1) = '" + Config.LG1 + "') . \n" + "?uri skos:prefLabel ?labelLg2 . \n"
-				+ "FILTER (lang(?labelLg2) = '" + Config.LG2 + "') . \n" + "?uri rdf:type ?typeOfObject . \n"
-
-				+ "BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w+$)', '$2' ) AS ?id) . \n"
-
-				+ "FILTER(STRENDS(STR(?series),'/operations/serie/" + idSeries + "')) . \n" + "}" + " ORDER BY ?id";
-	}
-
-	public static String getMultipleOrganizationsOld(String idSeries, IRI linkPredicate) {
-		return "SELECT ?id ?labelLg1 ?labelLg2\n" + "WHERE { \n" + "?series <" + linkPredicate + "> ?uri . \n"
-				+ "?uri dcterms:identifier  ?id . \n" + "?uri skos:prefLabel ?labelLg1 . \n"
-				+ "FILTER (lang(?labelLg1) = '" + Config.LG1 + "') . \n"
-				+ "OPTIONAL {?uri skos:prefLabel ?labelLg2 . \n" + "FILTER (lang(?labelLg2) = '" + Config.LG2
-				+ "')} . \n"
-
-				+ "FILTER(STRENDS(STR(?series),'/operations/serie/" + idSeries + "')) . \n"
-
-				+ "} \n" + "ORDER BY ?id";
-	}
-
-	public static String getFamilyOld(String idSeries) {
-
-		return "SELECT ?id ?labelLg1 ?labelLg2 \n" + " FROM <"+Config.OPERATIONS_GRAPH+"> \n" + "WHERE { \n"
-
-				+ "?family dcterms:hasPart ?series . \n" + "?family skos:prefLabel ?labelLg1 . \n"
-				+ "FILTER (lang(?labelLg1) = '" + Config.LG1 + "') . \n" + "?family skos:prefLabel ?labelLg2 . \n"
-				+ "FILTER (lang(?labelLg2) = '" + Config.LG2 + "') . \n"
-				+ "BIND(STRAFTER(STR(?family),'/famille/') AS ?id) . \n"
-
-				+ "FILTER(STRENDS(STR(?series),'/operations/serie/" + idSeries + "')) . \n" + "}"
-
-		;
-	}
-
-	public static String getOwnerOld(String uris) {
-		return "SELECT ?owner { \n" 
-				+ "?series dcterms:publisher ?owner . \n" 
-				+ "VALUES ?series { " + uris + " } \n"
-				+ "}";
-	}
-
-	public static String getCreatorsBySeriesUriOld(String uris) {
-		return "SELECT ?creators { \n" + "?series dc:creator ?creators . \n" 
-				+ "VALUES ?series { " + uris
-				+ " } \n" + "}";
-	}
-
-	public static String getCreatorsByIdOld(String id) {
-		return "SELECT ?creators\n" 
-				+ "WHERE { GRAPH <"+Config.OPERATIONS_GRAPH+"> { \n"
-				+ "?series a insee:StatisticalOperationSeries . \n" 
-				+ " FILTER(STRENDS(STR(?series),'/operations/serie/"+ id + "')) . \n" 
-				+ "?series dc:creator ?creators  . \n" + "} }";
-	}
-	
-	/**
-	 * return publishers id (publishers are organizations)
-	 * @param id
-	 * @return
-	 */
-	public static String getPublishersOld(String id) {
-		return "SELECT distinct ?publishers \n" 
-				
-				+ "FROM <"+Config.OPERATIONS_GRAPH+"> "
-				+ "FROM <"+Config.ORGANIZATIONS_GRAPH+"> "
-				+ "FROM <"+Config.ORG_INSEE_GRAPH+"> "
-				+ "WHERE { \n"
-					+ "?series a insee:StatisticalOperationSeries . \n" 
-					+ "?series dcterms:publisher ?uri  . \n" 
-					+ "?uri dcterms:identifier  ?publishers . \n" 
-					+ " FILTER(STRENDS(STR(?series),'/operations/serie/"+ id + "')) . \n" 
-				+ "} ";
-	}
-	
-	
 	private static void getSimpleAttr(String id) {
 
 		if (id != null) {
@@ -243,14 +123,6 @@ public class SeriesQueries {
  //   Using .flth files  //
 //////////////////////////
 	
-
-	private static final String ID_SERIES = "ID_SERIES";
-	private static final String URI_SERIES = "URI_SERIES";
-	private static final String ORGANIZATIONS_GRAPH = "ORGANIZATIONS_GRAPH";
-	private static final String OPERATIONS_GRAPH = "OPERATIONS_GRAPH";
-	private static final String ORG_INSEE_GRAPH = "ORG_INSEE_GRAPH";
-	private static final String LINK_PREDICATE = "LINK_PREDICATE";
-	
 	private static void initParams() {
 		params = new HashMap<>();
 		params.put("LG1", Config.LG1);
@@ -308,6 +180,7 @@ public class SeriesQueries {
 	
 	/**
 	 * @param idSeries
+	 * return publishers id (publishers are organizations)
 	 * @return String
 	 * @throws RmesException
 	 */	
@@ -342,4 +215,44 @@ public class SeriesQueries {
 		return buildSeriesRequest("getSeriesGeneratedWithQuery.ftlh", params);	
 	}
 	
+	/**
+	 * @param idSeries
+	 * @return String
+	 * @throws RmesException
+	 */	
+	public static String getOperations(String idSeries) throws RmesException {
+		if (params==null) {initParams();}
+		params.put(ID_SERIES, idSeries);
+		return buildSeriesRequest("getSeriesOperationsQuery.ftlh", params);	
+	}
+	
+	/**
+	 * @param idSeries, linkPredicate
+	 * @return String
+	 * @throws RmesException
+	 */	
+	public static String seriesLinks(String idSeries, IRI linkPredicate) throws RmesException {
+		if (params==null) {initParams();}
+		params.put(ID_SERIES, idSeries);
+		params.put(LINK_PREDICATE, linkPredicate);
+		return buildSeriesRequest("getSeriesLinksQuery.ftlh", params);	
+	}
+	
+	/**
+	 * @return String
+	 * @throws RmesException
+	 */	
+	public static String seriesWithSimsQuery() throws RmesException {
+		if (params==null) {initParams();}
+		return buildSeriesRequest("getSeriesWithSimsQuery.ftlh", params);	
+	}
+	
+	/**
+	 * @return String
+	 * @throws RmesException
+	 */	
+	public static String seriesQuery() throws RmesException {
+		if (params==null) {initParams();}
+		return buildSeriesRequest("getSeriesQuery.ftlh", params);	
+	}
 }
