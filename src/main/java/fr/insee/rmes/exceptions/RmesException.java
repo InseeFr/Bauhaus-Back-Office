2c582d73065fc3ceb0ded3d79e8c2ccafbad5d99
Alice Lambois <alice.lambois@insee.fr>
2021-01-22 09:12:24 +0100

    Fix issue with details and serializable

--- src/main/java/fr/insee/rmes/exceptions/RmesException.java
+++ src/main/java/fr/insee/rmes/exceptions/RmesException.java
@@ -14,7 +14,7 @@ public class RmesException extends Exception {
 	private static final long serialVersionUID = -7959158367542389147L;
 
 	private final int status;
-	private final transient JSONObject details;
+	private final String details;
 
 	/**
 	 *
@@ -25,63 +25,50 @@ public class RmesException extends Exception {
 	public RmesException(int status, String message, String details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(MESSAGE, message);
-		this.details.put(DETAILS_STRING,	details);		
+		this.details = createDetails(null, message, details);		
 	}
 
 	public RmesException(int status, String message, JSONArray details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(MESSAGE, message);
-		this.details.put(DETAILS_STRING, details.toString());
+		this.details = createDetails(null, message, details.toString());
 	}
 
 	public RmesException(int status, int errorCode, String message, String details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(CODE, errorCode);
-		this.details.put(MESSAGE, message);
-		this.details.put(DETAILS_STRING, details);
+		this.details = createDetails(errorCode, message, details);
 	}
 
 	public RmesException(int status, int errorCode, String details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(CODE, errorCode);
-		this.details.put(DETAILS_STRING, details);
+		this.details =  createDetails(errorCode, null, details);
 	}
 		
 	public RmesException(int status, int errorCode, JSONArray details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(CODE, errorCode);
-		this.details.put(DETAILS_STRING, details.toString());
+		this.details =  createDetails(errorCode, null, details.toString());
 	}
 
 	public RmesException(int status, int errorCode, String message, JSONArray details) {
 		super();
 		this.status = status;
-		this.details = new Details();
-		this.details.put(CODE, errorCode);
-		this.details.put(MESSAGE, message);
-		this.details.put(DETAILS_STRING, details.toString());
+		this.details =  createDetails(errorCode, message, details.toString());
 	}
 	
 	public RmesException(int status, int errorCode, String message, JSONObject details) {
 		super();
 		this.status = status;
-		this.details = details;
-		this.details.put(CODE, errorCode);
-		this.details.put(MESSAGE, message);
+		JSONObject det = details;
+		det.put(CODE, errorCode);
+		det.put(MESSAGE, message);
+		this.details=det.toString();
 	}
 
 	public RestMessage toRestMessage(){
-		return new RestMessage(this.status, this.getMessage(), this.details.toString());
+		return new RestMessage(this.status, this.getMessage(), this.details);
 	}
 
 	public int getStatus() {
@@ -89,11 +76,20 @@ public class RmesException extends Exception {
 	}
 
 	public String getDetails() {
-		return details.toString();
+		return details;
 	}
 
 	public String getMessageAndDetails2() {
 		return getMessage() + " " + details;
+	}
+	
+	private String createDetails(Integer errorCode, String message, String detailsParam) {
+		JSONObject det = new JSONObject();
+		if (errorCode != null) det.put(CODE, errorCode);
+		if (message != null) det.put(MESSAGE, message);
+		if (details != null) det.put(DETAILS_STRING, detailsParam);
+		return det.toString();
+		
 	}
 	
 }
