3748a66399f6e4d27ee7cfea531c9e9e2671ecf6
Alice Lambois <alice.lambois@insee.fr>
2021-02-01 13:25:38 +0100

    Fix & encoding

--- src/main/java/fr/insee/rmes/utils/XMLUtils.java
+++ src/main/java/fr/insee/rmes/utils/XMLUtils.java
@@ -34,16 +34,15 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import fr.insee.rmes.bauhaus_services.operations.documentations.DocumentationJsonMixIn;
 import fr.insee.rmes.model.operations.documentations.Documentation;
 
-
-
 public class XMLUtils {
 
 	static final Logger logger = LogManager.getLogger(XMLUtils.class);
 
-	public static final String toString(Document xml) throws TransformerFactoryConfigurationError, TransformerException  {
+	public static final String toString(Document xml)
+			throws TransformerFactoryConfigurationError, TransformerException {
 		TransformerFactory tf = TransformerFactory.newInstance();
 		tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-		tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, ""); 
+		tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
 		Transformer transformer = tf.newTransformer();
 		Writer out = new StringWriter();
 		transformer.transform(new DOMSource(xml), new StreamResult(out));
@@ -65,16 +64,15 @@ public class XMLUtils {
 
 		if (header != null && header.equals(MediaType.APPLICATION_XML)) {
 			mapper = new XmlMapper();
-		}
-		else {
+		} else {
 			mapper = new ObjectMapper();
-			// TODO : make it generic for all classes or change to 'produceXmlResponse'
+			// TODO : make it generic for all classes or change to
+			// 'produceXmlResponse'
 			mapper.addMixIn(Documentation.class, DocumentationJsonMixIn.class);
 		}
 		try {
 			response = mapper.writeValueAsString(obj);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
 		return encodeXml(response);
@@ -85,31 +83,29 @@ public class XMLUtils {
 		String response = "";
 		try {
 			response = mapper.writeValueAsString(obj);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
 		return encodeXml(response);
 	}
 
 	public static Document convertStringToDocument(String xmlStr) {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		// disable resolving of external DTD entities
 		factory.setAttribute(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
 		factory.setAttribute(XMLConstants.FEATURE_SECURE_PROCESSING, Boolean.TRUE);
-		DocumentBuilder builder;  
-		try  
-		{  
-			builder = factory.newDocumentBuilder();  
-			return builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
-		} catch (Exception e) {  
-			logger.error(e.getMessage());  
-		} 
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			return builder.parse(new InputSource(new StringReader(xmlStr)));
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
 		return null;
 	}
 
 	public static List<String> getTagValues(String text, String tag) {
-		final Pattern TAG_REGEX = Pattern.compile("<"+tag+">(.+?)</"+tag+">", Pattern.DOTALL);
+		final Pattern TAG_REGEX = Pattern.compile("<" + tag + ">(.+?)</" + tag + ">", Pattern.DOTALL);
 		final List<String> tagValues = new ArrayList<>();
 		final Matcher matcher = TAG_REGEX.matcher(text);
 		while (matcher.find()) {
@@ -117,12 +113,16 @@ public class XMLUtils {
 		}
 		return tagValues;
 	}
-	
-    private static String encodeXml(String response) {
-    	String ret = StringEscapeUtils.unescapeHtml4(response);
-    	ret = StringEscapeUtils.unescapeXml(ret);
-    	ret = ret.replace("&[^amp;]", "&amp;");
-    	return new String(ret.getBytes(), StandardCharsets.UTF_8);
-    }
+
+	private static String encodeXml(String response) {
+		String ret = StringEscapeUtils.unescapeXml(response);
+		ret = StringEscapeUtils.unescapeHtml4(ret);
+
+		final String regex = "&[^amp;]";
+
+		final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE | Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+		ret = pattern.matcher(ret).replaceAll("&amp;");
+		return new String(ret.getBytes(), StandardCharsets.UTF_8);
+	}
 
 }
