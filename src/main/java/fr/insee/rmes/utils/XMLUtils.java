c79d591c3e9ff62c721a682ed57b37b47b5c63c7
Alice Lambois <alice.lambois@insee.fr>
2021-02-08 13:51:14 +0100

    Try to fix issue

--- src/main/java/fr/insee/rmes/utils/XMLUtils.java
+++ src/main/java/fr/insee/rmes/utils/XMLUtils.java
@@ -33,6 +33,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import fr.insee.rmes.bauhaus_services.operations.documentations.DocumentationJsonMixIn;
 import fr.insee.rmes.model.operations.documentations.Documentation;
+import net.sf.saxon.TransformerFactoryImpl;
 
 public class XMLUtils {
 
@@ -45,7 +46,7 @@ public class XMLUtils {
 
 	public static final String toString(Document xml)
 			throws TransformerFactoryConfigurationError, TransformerException {
-		TransformerFactory tf = TransformerFactory.newDefaultInstance();
+		TransformerFactory tf = TransformerFactoryImpl.newDefaultInstance();
 		Transformer transformer = tf.newTransformer();
 		Writer out = new StringWriter();
 		transformer.transform(new DOMSource(xml), new StreamResult(out));
