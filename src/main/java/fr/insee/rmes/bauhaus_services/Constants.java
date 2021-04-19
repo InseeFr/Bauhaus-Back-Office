eca58e0a338e29aef37077af4fe7a1bf681a3642
Olivier Pucher <olivier.pucher@insee.fr>
2021-04-19 16:27:47 +0200

    Fix documents' language and updatedDate

--- src/main/java/fr/insee/rmes/bauhaus_services/Constants.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/Constants.java
@@ -58,6 +58,7 @@ public class Constants {
 	public static final String LABEL = "label";
 	public static final String LABEL_LG1 = "labelLg1";
 	public static final String LABEL_LG2 = "labelLg2";
+	public static final String LANG = "lang";
 	
 	/*M*/
 	public static final String MANAGER = "manager";
