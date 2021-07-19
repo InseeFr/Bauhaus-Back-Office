d24636257cfec479c46c09c053e40e725e8567e0
Alice Lambois <alice.lambois@insee.fr>
2021-07-19 11:11:29 +0200

    Update to spring 5

--- src/main/java/fr/insee/rmes/bauhaus_services/operations/series/SeriesUtils.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/operations/series/SeriesUtils.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/fr/insee/rmes/bauhaus_services/organizations/OrganizationUtils.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/organizations/OrganizationUtils.java
@@ -1,6 +1,6 @@
 package fr.insee.rmes.bauhaus_services.organizations;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
