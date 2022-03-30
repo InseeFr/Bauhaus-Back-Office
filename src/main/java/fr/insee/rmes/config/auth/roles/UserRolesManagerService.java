11288b0c8695961dda722e2a98b101bf9f887a99
Jérôme Bourgeois <jerome.bourgeois@insee.fr>
2022-03-30 10:17:55 +0200

    Fix : Sugoi replace Igesa
    exception for setAddRole and setDeleteRole
    getAgentsSugoi replace getAgents

--- src/main/java/fr/insee/rmes/config/auth/roles/UserRolesManagerService.java
+++ src/main/java/fr/insee/rmes/config/auth/roles/UserRolesManagerService.java
@@ -1,25 +1,20 @@
 package fr.insee.rmes.config.auth.roles;
 
-import java.io.IOException;
-
-import org.apache.http.auth.AuthenticationException;
-import org.apache.http.client.ClientProtocolException;
-
 import fr.insee.rmes.exceptions.RmesException;
 
 public interface UserRolesManagerService {
 	
-	public String checkLdapConnexion()  throws RmesException ;
+	
 	
 	public String getAuth(String body);
 	
 	public String getRoles() throws RmesException;
 	
-	public String getAgents() throws RmesException;
+	public String getAgentsSugoi() throws RmesException ;
 	
-	public void setAddRole(String role, String user) throws AuthenticationException, ClientProtocolException, IOException ;
+	public void setAddRole(String role, String user) throws  RmesException ;
 	
-	public void setDeleteRole(String roles, String user) throws AuthenticationException, ClientProtocolException, IOException;
+	public void setDeleteRole(String roles, String user) throws  RmesException;
 
 	public String checkSugoiConnexion() throws RmesException;
 	
