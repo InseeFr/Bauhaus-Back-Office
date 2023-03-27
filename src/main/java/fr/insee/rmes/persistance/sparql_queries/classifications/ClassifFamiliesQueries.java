1b4a0a1b52dcfae8298047b25795e9cf4672af44
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-03-27 15:24:26 +0200

    feat: finish rewrite Familiy Classification Sparql queries

--- src/main/java/fr/insee/rmes/persistance/sparql_queries/classifications/ClassifFamiliesQueries.java
+++ src/main/java/fr/insee/rmes/persistance/sparql_queries/classifications/ClassifFamiliesQueries.java
@@ -16,28 +16,19 @@ public class ClassifFamiliesQueries extends GenericQueries{
 
 	}
 	
-	public static String familyQuery(String id) {
-		return "SELECT ?prefLabelLg1 \n"
-			+ "WHERE { GRAPH<"+ config.getClassifFamiliesGraph() + "> { \n"
-			+ "?family skos:prefLabel ?prefLabelLg1 . \n"
-			+ "FILTER (lang(?prefLabelLg1) = '" + config.getLg1() + "') \n"
-			+ "FILTER(REGEX(STR(?family),'/familleDeNomenclatures/" + id + "')) } \n"
-			+ "} \n";	
+	public static String familyQuery(String id) throws RmesException {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("GRAPH", config.getClassifFamiliesGraph());
+		params.put("LG1", config.getLg1());
+		params.put("ID", id);
+		return FreeMarkerUtils.buildRequest("classifications/", "getFamily.ftlh", params);
 	}
 	
-	public static String familyMembersQuery(String id) {
-		return "SELECT DISTINCT ?id ?labelLg1 ?labelLg2 \n"
-			+ "WHERE { \n"
-			+ "?series xkos:belongsTo ?family . \n"
-			+ "?series skos:prefLabel ?labelLg1 . \n"
-			+ "FILTER (lang(?labelLg1) = '" + config.getLg1() + "') \n"
-			+ "OPTIONAL {?series skos:prefLabel ?labelLg2 . \n"
-			+ "FILTER (lang(?labelLg2) = '" + config.getLg2() + "') } \n"
-			+ "FILTER(REGEX(STR(?family),'/familleDeNomenclatures/" + id + "')) . \n"
-			+ "BIND(STRAFTER(STR(?series),'/codes/serieDeNomenclatures/') AS ?id) \n"
-			+ "} \n"
-			+ "ORDER BY ?labelLg1 ";	
+	public static String familyMembersQuery(String id) throws RmesException {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("LG1", config.getLg1());
+		params.put("LG2", config.getLg2());
+		params.put("ID", id);
+		return FreeMarkerUtils.buildRequest("classifications/", "getFamilyMembers.ftlh", params);
 	}
-
-	
 }
