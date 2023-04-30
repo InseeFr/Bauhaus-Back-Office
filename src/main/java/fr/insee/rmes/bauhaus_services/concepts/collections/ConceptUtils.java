271e889f030ca13201ef923c0e56692223cfbe61
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-04-30 21:55:01 +0200

    Be able to download a list of concepts #320

---
+++ src/main/java/fr/insee/rmes/bauhaus_services/concepts/collections/ConceptUtils.java
@@ -0,0 +1,25 @@
+package fr.insee.rmes.bauhaus_services.concepts.collections;
+
+import fr.insee.rmes.model.concepts.CollectionForExport;
+import fr.insee.rmes.utils.XMLUtils;
+import fr.insee.rmes.webservice.ConceptsCollectionsResources;
+import org.apache.commons.text.CaseUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConceptUtils {
+   public  static String getFileNameForExport(CollectionForExport collection, ConceptsCollectionsResources.Language lg){
+        if (lg == ConceptsCollectionsResources.Language.lg2){
+            return CaseUtils.toCamelCase(collection.getPrefLabelLg2(), false) + "-" + collection.getId();
+        }
+        return CaseUtils.toCamelCase(collection.getPrefLabelLg1(), false) + "-" + collection.getId();
+    }
+
+    public static  Map<String, String> convertCollectionInXml(CollectionForExport collection) {
+        String collectionXml = XMLUtils.produceXMLResponse(collection);
+        Map<String,String> xmlContent = new HashMap<>();
+        xmlContent.put("collectionFile",  collectionXml.replace("CollectionForExport", "Collection"));
+        return xmlContent;
+    }
+}
