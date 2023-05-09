be4d806f19008edf0a2707525057fe87383a43e3
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-09 11:13:11 +0200

    Feat/metadata nomenclature classification (#329)
    * feat: Display and Update the validation State of a classification #301
    
    * feat: rewrite Classification Families query into a FTLH file
    
    * feat: finish rewrite Familiy Classification Sparql queries

--- src/main/java/fr/insee/rmes/persistance/sparql_queries/classifications/ClassifFamiliesQueries.java
+++ src/main/java/fr/insee/rmes/persistance/sparql_queries/classifications/ClassifFamiliesQueries.java
@@ -1,42 +1,34 @@
 package fr.insee.rmes.persistance.sparql_queries.classifications;
 
+import fr.insee.rmes.bauhaus_services.rdf_utils.FreeMarkerUtils;
+import fr.insee.rmes.exceptions.RmesException;
 import fr.insee.rmes.persistance.sparql_queries.GenericQueries;
+
+import java.util.HashMap;
 
 public class ClassifFamiliesQueries extends GenericQueries{
 	
-	public static String familiesQuery() {
-		return "SELECT DISTINCT ?id ?label \n"
-			+ "WHERE { GRAPH <"+ config.getClassifFamiliesGraph() + "> { \n"
-			+ "?families skos:prefLabel ?label . \n"
-			+ "FILTER (lang(?label) = '" + config.getLg1() + "') \n"
-			+ "FILTER(REGEX(STR(?families),'/familleDeNomenclatures/')) . \n"
-			+ "BIND(STRAFTER(STR(?families),'/codes/familleDeNomenclatures/') AS ?id) } \n"
-			+ "} \n"
-			+ "ORDER BY ?label ";	
-	}
-	
-	public static String familyQuery(String id) {
-		return "SELECT ?prefLabelLg1 \n"
-			+ "WHERE { GRAPH<"+ config.getClassifFamiliesGraph() + "> { \n"
-			+ "?family skos:prefLabel ?prefLabelLg1 . \n"
-			+ "FILTER (lang(?prefLabelLg1) = '" + config.getLg1() + "') \n"
-			+ "FILTER(REGEX(STR(?family),'/familleDeNomenclatures/" + id + "')) } \n"
-			+ "} \n";	
-	}
-	
-	public static String familyMembersQuery(String id) {
-		return "SELECT DISTINCT ?id ?labelLg1 ?labelLg2 \n"
-			+ "WHERE { \n"
-			+ "?series xkos:belongsTo ?family . \n"
-			+ "?series skos:prefLabel ?labelLg1 . \n"
-			+ "FILTER (lang(?labelLg1) = '" + config.getLg1() + "') \n"
-			+ "OPTIONAL {?series skos:prefLabel ?labelLg2 . \n"
-			+ "FILTER (lang(?labelLg2) = '" + config.getLg2() + "') } \n"
-			+ "FILTER(REGEX(STR(?family),'/familleDeNomenclatures/" + id + "')) . \n"
-			+ "BIND(STRAFTER(STR(?series),'/codes/serieDeNomenclatures/') AS ?id) \n"
-			+ "} \n"
-			+ "ORDER BY ?labelLg1 ";	
-	}
+	public static String familiesQuery() throws RmesException {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("GRAPH", config.getClassifFamiliesGraph());
+		params.put("LG1", config.getLg1());
+		return FreeMarkerUtils.buildRequest("classifications/", "getFamilies.ftlh", params);
 
+	}
 	
+	public static String familyQuery(String id) throws RmesException {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("GRAPH", config.getClassifFamiliesGraph());
+		params.put("LG1", config.getLg1());
+		params.put("ID", id);
+		return FreeMarkerUtils.buildRequest("classifications/", "getFamily.ftlh", params);
+	}
+	
+	public static String familyMembersQuery(String id) throws RmesException {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("LG1", config.getLg1());
+		params.put("LG2", config.getLg2());
+		params.put("ID", id);
+		return FreeMarkerUtils.buildRequest("classifications/", "getFamilyMembers.ftlh", params);
+	}
 }
