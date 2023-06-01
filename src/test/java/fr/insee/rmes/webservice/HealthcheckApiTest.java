c33d02e9c86646a930b3eea17434190939e76398
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2023-06-01 16:23:10 +0200

    Many keycloak server (#340)
    * Fix(pom)  incrément version
    
    * fix(conflicts) after cherry pick
    
    * feat(many keycloaks) props and configuration for many keycloak
    
    * fix(compile errors) after merge
    
    * fix(many keycloak servers) bad props name to load configuration
    
    * fix(many keycloaks): correct bug with KeycloakServerZoneConfiguration constructor
    
    feat(valid token) offset by 1 s the test validity of access token to avoid the token to becom invalid just after validation
    
    fix(repo access token) keep many acce tokens in memory
    
    feat(version) increment pom version 😤
    
    Fix/334 many repositories with access tokens (#335)
    
    * feat(valid token) offset by 1 s the test validity of access token to avoid the token to becom invalid just after validation
    
    * fix(repo access token) keep many acce tokens in memory
    
    * feat(version) increment pom version 😤
    
    fix(oidc): correct bug with multiple repository connexions
    
    Merge branch 'main' into fix/334-many-repositories-with-access-tokens
    
    Merge pull request #336 from InseeFr/fix/334-many-repositories-with-access-tokens
    
    Fix/334 many repositories with access tokens
    
    fix healthcheck pbs and test it (#356)
    
    * fix(props) remove unused props
    
    * fix(healthcheck) test all repo even if first fail

---
+++ src/test/java/fr/insee/rmes/webservice/HealthcheckApiTest.java
@@ -0,0 +1,68 @@
+package fr.insee.rmes.webservice;
+
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryPublication;
+import fr.insee.rmes.stubs.RepositoryGestionStub;
+import fr.insee.rmes.stubs.RepositoryPublicationStub;
+import fr.insee.rmes.stubs.RepositoryPublicationStubInternalError;
+import net.sf.saxon.functions.StringJoin;
+import org.junit.jupiter.api.Test;
+
+import java.util.StringJoiner;
+
+import static fr.insee.rmes.stubs.RepositoryPublicationStubInternalError.ERROR_MESSAGE;
+import static fr.insee.rmes.webservice.HealthcheckApi.KO_STATE;
+import static fr.insee.rmes.webservice.HealthcheckApi.OK_STATE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthcheckApiTest {
+
+    @Test
+    void checkDatabaseTest_success() {
+        //Given
+        RepositoryGestion repoGestionStub=new RepositoryGestionStub();
+        RepositoryPublication repoPublicationStub=new RepositoryPublicationStub();
+        var healthcheckApi=new HealthcheckApi(repoGestionStub, repoPublicationStub, null);
+        StringJoiner errorMessage = new StringJoiner(" ");
+        StringJoiner stateResult = new StringJoiner(" ");
+
+        //When
+        healthcheckApi.checkDatabase(errorMessage, stateResult);
+
+        //Then
+        assertThat(stateResult).hasToString(
+                "Database connexion \n"
+                +" "+" - Publication Z"+" "+OK_STATE
+                +" "+" - Publication I"+" "+OK_STATE
+                +" "+" - Gestion"+" "+OK_STATE
+
+        );
+        assertThat(errorMessage.toString()).isEmpty();
+    }
+
+    @Test
+    void checkDatabaseTest_withInternalPublicationError() {
+        //Given
+        RepositoryGestion repoGestionStub=new RepositoryGestionStub();
+        RepositoryPublication repoPublicationStub=new RepositoryPublicationStubInternalError();
+        var healthcheckApi=new HealthcheckApi(repoGestionStub, repoPublicationStub, null);
+        StringJoiner errorMessage = new StringJoiner(" ");
+        StringJoiner stateResult = new StringJoiner(" ");
+
+        //When
+        healthcheckApi.checkDatabase(errorMessage, stateResult);
+
+        //Then
+        assertThat(stateResult).hasToString(
+                "Database connexion \n"
+                        +" "+" - Publication Z"+" "+OK_STATE
+                        +" "+" - Publication I"+" "+KO_STATE
+                        +" "+" - Gestion"+" "+OK_STATE
+
+        );
+        assertThat(errorMessage).hasToString("- Publication I "+null+ " \n");
+    }
+
+
+}
