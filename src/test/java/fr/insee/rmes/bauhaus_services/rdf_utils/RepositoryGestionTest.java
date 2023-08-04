3e866a2d358242f5486795d04e1c53e74d9adf80
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-08-04 10:23:54 +0200

    Feat/clear graph endpoint (#346)
    * feat: create endpoint in order to delete an entire graph
    
    * fix: remove unused method
    
    * feat: be able to execute a trig file
    
    * fix: get data from prod database when calling clear endpoint

---
+++ src/test/java/fr/insee/rmes/bauhaus_services/rdf_utils/RepositoryGestionTest.java
@@ -0,0 +1,51 @@
+package fr.insee.rmes.bauhaus_services.rdf_utils;
+
+import fr.insee.rmes.config.Config;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class RepositoryGestionTest {
+
+    @InjectMocks
+    RepositoryGestion repositoryGestion;
+
+    @Mock
+    Config config;
+
+    @Mock
+    RepositoryUtils repositoryUtils;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+   @Test
+    void shouldCallRepositoryClear() throws IOException {
+        when(config.getRdfServerGestion()).thenReturn("rdfServerGestion");
+        when(config.getRepositoryIdGestion()).thenReturn("repositoryIdGestion");
+
+        Repository mockRepository = mock(Repository.class);
+        RepositoryConnection mockRepositoryConnection = mock(RepositoryConnection.class);
+
+        when(repositoryUtils.initRepository(anyString(), anyString())).thenReturn(mockRepository);
+        when(mockRepository.getConnection()).thenReturn(mockRepositoryConnection);
+        repositoryGestion.clearGraph();
+
+       verify(mockRepositoryConnection, times(1)).clear();
+       verify(mockRepositoryConnection, times(1)).add(any(File.class), eq(RDFFormat.TRIG));
+    }
+
+}
