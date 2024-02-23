a87bab3b1ccec0b3e21f8d8e210813c4a3a791f8
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-02-23 14:34:49 +0100

    refactor(to make tests fail) 😭

--- src/main/java/fr/insee/rmes/config/auth/security/DefaultSecurityContext.java
+++ src/main/java/fr/insee/rmes/config/auth/security/DefaultSecurityContext.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Optional;
@@ -16,36 +15,35 @@ import java.util.Optional;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 @ConditionalOnExpression("!'PROD'.equalsIgnoreCase('${fr.insee.rmes.bauhaus.env}')")
 public class DefaultSecurityContext {
 
-	private static final Logger logger = LoggerFactory.getLogger(DefaultSecurityContext.class);
-	private final boolean requiresSsl;
+    private static final Logger logger = LoggerFactory.getLogger(DefaultSecurityContext.class);
+    private final boolean requiresSsl;
 
-	public DefaultSecurityContext(@Value("${fr.insee.rmes.bauhaus.force.ssl}") boolean requiresSsl){
-		this.requiresSsl=requiresSsl;
-	}
+    public DefaultSecurityContext(@Value("${fr.insee.rmes.bauhaus.force.ssl}") boolean requiresSsl) {
+        this.requiresSsl = requiresSsl;
+    }
 
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.cors(withDefaults())
-				.authorizeRequests().anyRequest().permitAll();
-		if (requiresSsl) {
-			http.antMatcher("/**").requiresChannel().anyRequest().requiresSecure();
-		}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement().disable()
+                .cors(withDefaults())
+                .authorizeRequests().anyRequest().permitAll();
+        if (requiresSsl) {
+            http.antMatcher("/**").requiresChannel().anyRequest().requiresSecure();
+        }
 
-		logger.info("Default authentication activated - no auth ");
+        logger.info("Default authentication activated - no auth ");
 
-		return http.build();
+        return http.build();
 
-	}
+    }
 
-	@Bean
-	public UserDecoder getUserProvider() {
-		return principal -> Optional.of(User.FAKE_USER);
-	}
+    @Bean
+    public UserDecoder getUserProvider() {
+        return principal -> Optional.of(User.FAKE_USER);
+    }
 
 }
