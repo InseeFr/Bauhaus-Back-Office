b36813cb63910be38d758f52bc824bd8bd54fb61
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2024-02-23 11:34:38 +0100

    test: put codelist/id for admin
    has to be OK

---
+++ src/test/java/fr/insee/rmes/integration/authorizations/TestCodeListsResourcesEnvProd.java
@@ -0,0 +1,75 @@
+package fr.insee.rmes.integration.authorizations;
+
+import fr.insee.rmes.bauhaus_services.CodeListService;
+import fr.insee.rmes.bauhaus_services.OperationsDocumentationsService;
+import fr.insee.rmes.bauhaus_services.OperationsService;
+import fr.insee.rmes.bauhaus_services.StampAuthorizationChecker;
+import fr.insee.rmes.config.Config;
+import fr.insee.rmes.config.auth.UserProviderFromSecurityContext;
+import fr.insee.rmes.config.auth.roles.Roles;
+import fr.insee.rmes.config.auth.security.CommonSecurityConfiguration;
+import fr.insee.rmes.config.auth.security.DefaultSecurityContext;
+import fr.insee.rmes.config.auth.security.OpenIDConnectSecurityContext;
+import fr.insee.rmes.webservice.CodeListsResources;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static fr.insee.rmes.integration.authorizations.TokenForTestsConfiguration.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CodeListsResources.class,
+        properties = {"fr.insee.rmes.bauhaus.env=PROD",
+                "jwt.stamp-claim=" + STAMP_CLAIM,
+                "jwt.role-claim=" + ROLE_CLAIM,
+                "jwt.id-claim=" + ID_CLAIM,
+                "jwt.role-claim.roles=" + KEY_FOR_ROLES_IN_ROLE_CLAIM,
+                "logging.level.org.springframework.security=DEBUG",
+                "logging.level.org.springframework.security.web.access=TRACE",
+                "logging.level.fr.insee.rmes.config.auth=TRACE",
+                "fr.insee.rmes.bauhaus.activeModules=operations"}
+)
+@Import({Config.class,
+        OpenIDConnectSecurityContext.class,
+        DefaultSecurityContext.class,
+        CommonSecurityConfiguration.class,
+        UserProviderFromSecurityContext.class})
+public class TestCodeListsResourcesEnvProd {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private JwtDecoder jwtDecoder;
+    @MockBean
+    protected OperationsDocumentationsService documentationsService;
+    @MockBean
+    StampAuthorizationChecker stampAuthorizationChecker;
+    @MockBean
+    private CodeListService codeListService;
+    private final String idep = "xxxxux";
+    private final String timbre = "XX59-YYY";
+
+    int codeListId=10;
+
+    @Test
+    void putCodeListAdmin_ok() throws Exception {
+        configureJwtDecoderMock(jwtDecoder, idep, timbre, List.of("Administrateur_RMESGNCS"));
+
+        mvc.perform(put("/codeList/" + codeListId).header("Authorization", "Bearer toto")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content("{\"id\": \"1\"}"))
+                .andExpect(status().isOk());
+    }
+
+
+}
