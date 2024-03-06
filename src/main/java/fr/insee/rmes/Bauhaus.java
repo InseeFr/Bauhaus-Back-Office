01fd1b137470fc9a0b112d7e171f355f13dcb40b
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-03-06 10:40:22 +0100

    refactor(upgrade spring boot to 3.2) clean dependencies, openAPI config and main class

--- src/main/java/fr/insee/rmes/Bauhaus.java
+++ src/main/java/fr/insee/rmes/Bauhaus.java
@@ -4,17 +4,10 @@ import fr.insee.rmes.config.PropertiesLogger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class Bauhaus extends SpringBootServletInitializer {
-
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-        return configureApplicationBuilder(applicationBuilder);
-    }
+public class Bauhaus{
 
     public static void main(String[] args) {
         configureApplicationBuilder(new SpringApplicationBuilder()).build().run(args);
