8078ac8c728456f2c2b643baf0dab6abe8271021
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-03-06 10:40:22 +0100

    refactor(upgrade spring boot to 3.2) clean dependencies, openAPI config and main class

--- src/main/java/fr/insee/rmes/config/swagger/OpenApiConfiguration.java
+++ src/main/java/fr/insee/rmes/config/swagger/OpenApiConfiguration.java
@@ -2,40 +2,14 @@ package fr.insee.rmes.config.swagger;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SecurityScheme(//add Authorize button in swagger
-	    name = "bearerAuth",
-	    type = SecuritySchemeType.HTTP,
-	    bearerFormat = "JWT",
-	    scheme = "bearer"
-	)
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenApiConfiguration   {
-
-	private static final  Logger logger = LoggerFactory.getLogger(OpenApiConfiguration.class);
-
-	
-	@Bean
-	public OpenAPI customOpenAPI(@Value("${fr.insee.rmes.bauhaus.api.ssl}") boolean swaggerUseSSL, @Value("${fr.insee.rmes.bauhaus.api.host}") String swaggerHost, @Value("${fr.insee.rmes.bauhaus.api.basepath}") String swaggerBasepath, @Value("${fr.insee.rmes.bauhaus.version}") String projectVersion) {
-		Server server = new Server();
-		var swaggerUrl = (swaggerUseSSL ? "https" : "http")+"://"+ swaggerHost + "/" + swaggerBasepath;
-		server.setUrl(swaggerUrl);
-		logger.info("____________________SWAGGER CONFIG : {} _________________________________________________", swaggerUrl);
-		return new OpenAPI()
-				.addServersItem(server)
-				.info(new Info()
-						.title("Bauhaus API")
-						.description("Rest Endpoints and services Integration used by Bauhaus")
-						.version(projectVersion)
-				);
-	}
-
 }
