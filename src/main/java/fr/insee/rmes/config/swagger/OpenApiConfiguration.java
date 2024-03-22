4c12035e0594e9a3283e262fccc9a4c080e4fe2e
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-03-22 15:41:53 +0100

    fix(openapi docs should print app version)

--- src/main/java/fr/insee/rmes/config/swagger/OpenApiConfiguration.java
+++ src/main/java/fr/insee/rmes/config/swagger/OpenApiConfiguration.java
@@ -2,6 +2,10 @@ package fr.insee.rmes.config.swagger;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +16,24 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class OpenApiConfiguration   {
+
+    public static final String DESCRIPTION="Back office de Bauhaus (rmesgncs)";
+    public static final String TITLE="Bauhaus";
+
+    @Bean
+    public OpenAPI springShopOpenAPI(@Value("${fr.insee.rmes.bauhaus.version}") String version) {
+        return new OpenAPI()
+                .info((new Info()).version(version));
+    }
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .version(appVersion)
+                        .description(DESCRIPTION)
+                );
+    }
+
 }
