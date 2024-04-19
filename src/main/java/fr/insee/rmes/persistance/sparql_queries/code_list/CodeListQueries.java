a3b2fafa3a0a9926599695de1d77dca1b3e70e91
Emmanuel <demey.emmanuel@gmail.com>
2024-04-19 14:15:13 +0200

    fix: merge

--- src/main/java/fr/insee/rmes/persistance/sparql_queries/code_list/CodeListQueries.java
+++ src/main/java/fr/insee/rmes/persistance/sparql_queries/code_list/CodeListQueries.java
@@ -49,15 +49,7 @@ public class CodeListQueries extends GenericQueries {
 		return FreeMarkerUtils.buildRequest(CODES_LIST, "getBroaderNarrowerCloseMatch.ftlh", params);
 	}
 
-	public static String getDetailedCodes(String notation, boolean partial, List<String> search, int page, Integer perPage) throws RmesException {
-		HashMap<String, Object> params = new HashMap<>();
-		int perPageValue = getPerPageConfiguration(perPage);
-		initParams(params);
-		params.put(NOTATION, notation);
-		params.put(PARTIAL, partial);
-		params.put(CODE_LIST_BASE_URI, config.getCodeListBaseUri());
-
-  private static void addSearchPredicates(Map<String, Object> params, List<String> search) {
+	private static void addSearchPredicates(Map<String, Object> params, List<String> search) {
 		if(search != null){
 			search.forEach(s -> {
 				if(!StringUtils.isEmpty(s)){
