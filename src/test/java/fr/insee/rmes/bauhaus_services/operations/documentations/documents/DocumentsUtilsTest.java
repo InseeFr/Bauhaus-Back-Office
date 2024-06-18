911c5bf73ce9c55d3e80ad63969c723d42fa0a47
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-06-18 08:51:21 +0200

    feat: be able to control file name (#648)
    * feat: be able to control file name
    
    * refactor(Documents creations and updates) test more invalid names and use mockito instead of spring
    
    * feat: improve document name validation
    
    * refactor(use records)example for DocumentsImpl.java
    
    ---------
    
    Co-authored-by: Fabrice B <fabrice.bibonne@insee.fr>

--- src/test/java/fr/insee/rmes/bauhaus_services/operations/documentations/documents/DocumentsUtilsTest.java
+++ src/test/java/fr/insee/rmes/bauhaus_services/operations/documentations/documents/DocumentsUtilsTest.java
@@ -2,28 +2,32 @@ package fr.insee.rmes.bauhaus_services.operations.documentations.documents;
 
 import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
 import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.exceptions.RmesNotAcceptableException;
 import fr.insee.rmes.persistance.sparql_queries.operations.documentations.DocumentsQueries;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DocumentsUtilsTest {
 
-    @MockBean
-    RepositoryGestion repositoryGestion;
+    @Mock
+    RepositoryGestion repositoryGestion = new RepositoryGestion();
 
-    @Autowired
+    @InjectMocks
     DocumentsUtils documentsUtils;
 
     @Test
@@ -38,5 +42,32 @@ class DocumentsUtilsTest {
             JSONArray response = documentsUtils.getListDocumentLink("1", "2", "en");
             Assertions.assertEquals(response.get(0), document);
         }
+    }
+
+    @CsvSource({
+            ",'{\"code\":361,\"message\":\"Empty fileName\"}'",
+            "'','{\"code\":361,\"message\":\"Empty fileName\"}'",
+            "'invalid@name!', '{\"code\":362,\"details\":\"invalid@name!\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+            "'.', '{\"code\":362,\"details\":\".\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+            "'..', '{\"code\":362,\"details\":\"..\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+            "'...', '{\"code\":362,\"details\":\"...\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+            "'-', '{\"code\":362,\"details\":\"-\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+            "'-.', '{\"code\":362,\"details\":\"-.\",\"message\":\"FileName contains forbidden characters, please use only Letters, Numbers, Underscores and Hyphens\"}'",
+   })
+    @ParameterizedTest
+    void test_checkFileNameValidity_throwsWhenNameInvalid(String fileName, String exceptionDetail) {
+        RmesNotAcceptableException exception = assertThrows(RmesNotAcceptableException.class, () -> {
+            documentsUtils.checkFileNameValidity(fileName);
+        });
+        assertEquals(exception.getDetails(), exceptionDetail);
+    }
+
+
+    @Test
+    void testFileNameIsValid() {
+        String fileName = "valid_file-name.txt";
+        assertDoesNotThrow(() -> {
+            documentsUtils.checkFileNameValidity(fileName);
+        });
     }
 }
