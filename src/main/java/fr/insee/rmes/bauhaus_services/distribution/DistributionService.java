b029f8635e453e98e09576a6be2b57332b162247
G Tan Sndil <72496065+GtanSndil@users.noreply.github.com>
2024-06-28 15:01:30 +0200

    Revert "495 : etre capable de supprimer une distribution (#649)" (#673)
    This reverts commit dd45cd3261f693fcaf513bd5eb5a7ecbef21872d.

--- src/main/java/fr/insee/rmes/bauhaus_services/distribution/DistributionService.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/distribution/DistributionService.java
@@ -2,7 +2,6 @@ package fr.insee.rmes.bauhaus_services.distribution;
 
 import fr.insee.rmes.exceptions.RmesException;
 import fr.insee.rmes.model.dataset.PatchDistribution;
-import org.springframework.http.ResponseEntity;
 
 public interface DistributionService {
 
@@ -17,6 +16,4 @@ public interface DistributionService {
     String publishDistribution(String id) throws RmesException;
 
     void patchDistribution(String distributionId, PatchDistribution distribution) throws RmesException;
-
-    void deleteDistributionId(String distributionId) throws RmesException;
 }
