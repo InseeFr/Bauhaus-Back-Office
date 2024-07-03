0c234967f65ac14e58be621c6d67149d187dc58a
Hugo Bouttes <hugo.bouttes@insee.fr>
2024-07-03 11:13:15 +0200

    fix : add Component to idGenerator

--- src/main/java/fr/insee/rmes/utils/IdGenerator.java
+++ src/main/java/fr/insee/rmes/utils/IdGenerator.java
@@ -1,7 +1,10 @@
 package fr.insee.rmes.utils;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class IdGenerator {
     public String generateNextId() {
         UUID uuid = UUID.randomUUID();
