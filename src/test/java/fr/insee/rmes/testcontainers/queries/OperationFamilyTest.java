88fc89f7a0913c1e5ce8caa5932040577c67a4dd
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-07-05 14:03:45 +0200

    Testcontainers (#677)
    * feat: add testcontainer
    
    * feat: add test container
    
    * feat: add maven profil

---
+++ src/test/java/fr/insee/rmes/testcontainers/queries/OperationFamilyTest.java
@@ -0,0 +1,23 @@
+package fr.insee.rmes.testcontainers.queries;
+
+import fr.insee.rmes.bauhaus_services.OperationsService;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class OperationFamilyTest extends WithGraphDBContainer{
+
+    @Autowired
+    OperationsService operationService;
+
+    @Test
+    void getAllFamilies() throws Exception {
+        importTrigFile("all-operations-and-indicators.trig");
+        String result = operationService.getFamilies();
+        assertEquals(new JSONArray(result).length(), 56);
+    }
+}
