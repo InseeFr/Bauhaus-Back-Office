a3cbbece8ac8566e9eee37f886adafc085ede812
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-07-12 10:13:09 +0200

    feat: remove isRequiredBy statement for structure (#695)

--- src/main/java/fr/insee/rmes/bauhaus_services/structures/utils/StructureUtils.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/structures/utils/StructureUtils.java
@@ -255,7 +255,6 @@ public class StructureUtils extends RdfService {
         RdfUtils.addTripleString(structureIri, RDFS.LABEL, structure.getLabelLg2(), config.getLg2(), model, graph);
         RdfUtils.addTripleString(structureIri, RDFS.COMMENT, structure.getDescriptionLg1(), config.getLg1(), model, graph);
         RdfUtils.addTripleString(structureIri, RDFS.COMMENT, structure.getDescriptionLg2(), config.getLg2(), model, graph);
-        RdfUtils.addTripleString(structureIri, DCTERMS.IS_REQUIRED_BY, structure.getIsRequiredBy(), model, graph);
 
         RdfUtils.addTripleString(structureIri, DC.CREATOR, structure.getCreator(), model, graph);
         structure.getContributor().forEach(contributor -> RdfUtils.addTripleString(structureIri, DC.CONTRIBUTOR, contributor, model, graph));
