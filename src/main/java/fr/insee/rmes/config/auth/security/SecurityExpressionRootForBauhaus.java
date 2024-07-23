e27882aeee23645f7b17de527f60e53d307f3bf1
Hugo Bouttes <hugo.bouttes@insee.fr>
2024-07-23 15:26:12 +0200

    feat : add canUpdateSerie and getAccessPrivileges

--- src/main/java/fr/insee/rmes/config/auth/security/SecurityExpressionRootForBauhaus.java
+++ src/main/java/fr/insee/rmes/config/auth/security/SecurityExpressionRootForBauhaus.java
@@ -3,10 +3,15 @@ package fr.insee.rmes.config.auth.security;
 import fr.insee.rmes.bauhaus_services.StampAuthorizationChecker;
 import fr.insee.rmes.config.auth.roles.Roles;
 import fr.insee.rmes.config.auth.user.Stamp;
+import fr.insee.rmes.exceptions.RmesException;
 import fr.insee.rmes.exceptions.RmesRuntimeBadRequestException;
+import fr.insee.rmes.external.services.rbac.AccessPrivileges;
+import fr.insee.rmes.external.services.rbac.RBACService;
+import fr.insee.rmes.model.rbac.RBAC;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.security.access.expression.SecurityExpressionOperations;
 import org.springframework.security.access.expression.SecurityExpressionRoot;
@@ -19,6 +24,12 @@ import java.util.function.BiPredicate;
 import static java.util.Objects.requireNonNull;
 
 public class SecurityExpressionRootForBauhaus implements MethodSecurityExpressionOperations, SecurityExpressionOperations {
+
+    @Autowired
+    RBACService rbacService;
+
+    @Autowired
+    UserDecoder userDecoder;
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityExpressionRootForBauhaus.class);
 
@@ -227,6 +238,14 @@ public class SecurityExpressionRootForBauhaus implements MethodSecurityExpressio
   
     private Optional<Stamp> getStamp() {
         return this.stampFromPrincipal.findStamp(methodSecurityExpressionRoot.getPrincipal());
+    }
+
+    public boolean canUpdateSerie(String serieId) throws RmesException{
+        return getAccessPrivileges().isGranted(RBAC.Privilege.UPDATE).on(RBAC.Module.SERIE).withId(serieId);
+    }
+
+    private AccessPrivileges getAccessPrivileges() throws RmesException {
+        return rbacService.computeRbac(userDecoder.fromPrincipal(methodSecurityExpressionRoot.getPrincipal()).get().roles());
     }
 
 }
