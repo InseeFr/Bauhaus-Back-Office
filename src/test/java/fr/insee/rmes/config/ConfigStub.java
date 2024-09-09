454986a415cc407261e7c52f4f27cbfe69c17eea
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-09-09 16:50:28 +0200

    fix: solve issue when creating a structure and when ID is null (#739)
    Co-authored-by: Fabrice B <fabrice.bibonne@insee.fr>

--- src/test/java/fr/insee/rmes/config/ConfigStub.java
+++ src/test/java/fr/insee/rmes/config/ConfigStub.java
@@ -26,4 +26,20 @@ public class ConfigStub extends Config {
     public String getOrgInseeGraph() {
         return "http://rdf.insee.fr/graphes/organisations/insee";
     }
+
+    //TODO
+    @Override
+    public String getStructuresComponentsGraph() {
+        return "http://rdf.insee.fr/graphes/composants";
+    }
+
+    @Override
+    public String getStructuresGraph() {
+        return "http://rdf.insee.fr/graphes/structures";
+    }
+
+    @Override
+    public String getCodeListGraph() {
+        return "http://rdf.insee.fr/graphes/codes";
+    }
 }
