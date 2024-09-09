a3db4a933d1e8f57ce352bbd0ad32b6140f957ce
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-09-09 16:50:28 +0200

    fix: solve issue when creating a structure and when ID is null (#739)
    Co-authored-by: Fabrice B <fabrice.bibonne@insee.fr>

---
+++ src/test/java/fr/insee/rmes/testcontainers/queries/sparql_queries/structures/StructureQueriesTest.java
@@ -0,0 +1,30 @@
+package fr.insee.rmes.testcontainers.queries.sparql_queries.structures;
+
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryInitiator;
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryUtils;
+import fr.insee.rmes.config.ConfigStub;
+import fr.insee.rmes.persistance.sparql_queries.structures.StructureQueries;
+import fr.insee.rmes.testcontainers.queries.WithGraphDBContainer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class StructureQueriesTest extends WithGraphDBContainer {
+    RepositoryGestion repositoryGestion = new RepositoryGestion(getRdfGestionConnectionDetails(), new RepositoryUtils(null, RepositoryInitiator.Type.DISABLED));
+
+    @BeforeAll
+    static void initData(){
+        container.withTrigFiles("jeuxDeDonnees-pour-tests.trig");
+    }
+
+    @Test
+    void should_return_false_if_existing_structure_with_same_components_and_id_null() throws Exception {
+        StructureQueries.setConfig(new ConfigStub());
+        boolean result = repositoryGestion.getResponseAsBoolean(StructureQueries.checkUnicityStructure(null, List.of("2").toArray(new String[0])));
+        assertFalse(result);
+    }
+}
