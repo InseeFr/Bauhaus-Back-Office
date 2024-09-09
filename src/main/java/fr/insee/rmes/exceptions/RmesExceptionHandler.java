608469c3b00fe0a139460e52ee084efbb50e1ac6
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-09-09 16:53:18 +0200

    fix: review Exception handler for SIMS during publishing (#742)
    * fix: review Exception handler for SIMS during publishing
    
    * feat: change return type

--- src/main/java/fr/insee/rmes/exceptions/RmesExceptionHandler.java
+++ src/main/java/fr/insee/rmes/exceptions/RmesExceptionHandler.java
@@ -3,12 +3,18 @@ package fr.insee.rmes.exceptions;
 import fr.insee.rmes.webservice.codesLists.CodeListsResources;
 import fr.insee.rmes.webservice.dataset.DatasetResources;
 import fr.insee.rmes.webservice.distribution.DistributionResources;
+import fr.insee.rmes.webservice.operations.MetadataReportResources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice(assignableTypes = {DatasetResources.class, DistributionResources.class, CodeListsResources.class})
+@ControllerAdvice(assignableTypes = {
+        DatasetResources.class,
+        DistributionResources.class,
+        CodeListsResources.class,
+        MetadataReportResources.class
+})
 public class RmesExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({RmesBadRequestException.class, RmesNotFoundException.class, RmesNotAcceptableException.class, RmesUnauthorizedException.class})
