08ad74cb973cc263033709eb34af6d54c116d2ae
Hugo Bouttes <hugo.bouttes@insee.fr>
2024-09-11 16:52:37 +0200

    fix : change identifier access

--- src/main/java/fr/insee/rmes/config/swagger/model/code_list/Id.java
+++ src/main/java/fr/insee/rmes/config/swagger/model/code_list/Id.java
@@ -1,9 +1,9 @@
 package fr.insee.rmes.config.swagger.model.code_list;
 
 public class Id {
-    public String id;
+    private String identifier;
 
-    public Id(String id) {
-        this.id = id;
+    public Id(String identifier) {
+        this.identifier = identifier;
     }
 }
