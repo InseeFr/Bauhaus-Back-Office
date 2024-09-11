46ec7a4b6841135a5bb8e76e8634f75231df7623
Hugo <103732523+HugoBouttes@users.noreply.github.com>
2024-09-11 17:24:45 +0200

    6 change code list answer (#751)
    * feat : add Id Codelist class
    
    * fix change ResponseEntity<String> in ResponseEntity<Id>
    
    * fix : change identifier access
    
    * fix :change Id type in publishFullCodeList

---
+++ src/main/java/fr/insee/rmes/config/swagger/model/code_list/Id.java
@@ -0,0 +1,13 @@
+package fr.insee.rmes.config.swagger.model.code_list;
+
+public class Id {
+    private String identifier;
+
+    public Id(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+}
