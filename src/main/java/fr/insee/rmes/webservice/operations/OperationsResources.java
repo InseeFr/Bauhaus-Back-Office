ef5681ed196adc94179b8c0ea7749a5f2da8a8de
Emmanuel <demey.emmanuel@gmail.com>
2024-09-25 12:32:15 +0100

    fix: review

--- src/main/java/fr/insee/rmes/webservice/operations/OperationsResources.java
+++ src/main/java/fr/insee/rmes/webservice/operations/OperationsResources.java
@@ -27,25 +27,19 @@ import org.springframework.web.bind.annotation.*;
 public class OperationsResources extends OperationsCommonResources {
 
 
-	/***************************************************************************************************
-	 * OPERATIONS
-	 ******************************************************************************************************/
-
 	@GetMapping(value = "/operations", produces = MediaType.APPLICATION_JSON_VALUE)
 	@io.swagger.v3.oas.annotations.Operation(operationId = "getOperations", summary = "List of operations", responses = {
 			@ApiResponse(content = @Content(schema = @Schema(type = "array", implementation = IdLabelAltLabel.class))) })
-	public ResponseEntity<Object> getOperations() throws RmesException {
-		String jsonResultat = operationsService.getOperations();
-		return ResponseEntity.status(HttpStatus.OK).body(jsonResultat);
+	public ResponseEntity<String> getOperations() throws RmesException {
+		return ResponseEntity.status(HttpStatus.OK).body(operationsService.getOperations());
 	}
 
 	@GetMapping(value = "/operation/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE })
 	@io.swagger.v3.oas.annotations.Operation(operationId = "getOperationByID", summary = "Get an operation", responses = {
 			@ApiResponse(content = @Content(/* mediaType = "application/json", */ schema = @Schema(implementation = Operation.class))) })
-	public ResponseEntity<Object> getOperationByID(@PathVariable(Constants.ID) String id,
+	public ResponseEntity<String> getOperationByID(@PathVariable(Constants.ID) String id,
 			@Parameter(hidden = true) @RequestHeader(required=false) String accept) throws RmesException {
-		String resultat;
 		if (accept != null && accept.equals(MediaType.APPLICATION_XML_VALUE)) {
 			return ResponseEntity.status(HttpStatus.OK).body(XMLUtils.produceXMLResponse(operationsService.getOperationById(id)));
 		} else {
@@ -53,49 +47,30 @@ public class OperationsResources extends OperationsCommonResources {
 		}
 	}
 
-	/**
-	 * UPDATE
-	 * 
-	 * @param id
-	 * @param body
-	 * @return
-	 */
 	@PreAuthorize("hasAnyRole(T(fr.insee.rmes.config.auth.roles.Roles).ADMIN " + ", T(fr.insee.rmes.config.auth.roles.Roles).SERIES_CONTRIBUTOR "
 			+ ", T(fr.insee.rmes.config.auth.roles.Roles).CNIS)")
 	@PutMapping(value = "/operation/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@io.swagger.v3.oas.annotations.Operation(operationId = "setOperationById", summary = "Update an operation")
-	public ResponseEntity<Object> setOperationById(@PathVariable(Constants.ID) String id,
+	public ResponseEntity<Void> setOperationById(@PathVariable(Constants.ID) String id,
 			@Parameter(description = "Operation to update", required = true, content = @Content(schema = @Schema(implementation = Operation.class))) @RequestBody String body) throws RmesException {
 
 		operationsService.setOperation(id, body);
 		return ResponseEntity.noContent().build();
 	}
 
-	/**
-	 * CREATE
-	 * 
-	 * @param body
-	 * @return
-	 */
 	@PreAuthorize("hasAnyRole(T(fr.insee.rmes.config.auth.roles.Roles).ADMIN " + ", T(fr.insee.rmes.config.auth.roles.Roles).SERIES_CONTRIBUTOR)")
 	@PostMapping(value = "/operation", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@io.swagger.v3.oas.annotations.Operation(operationId = "createOperation", summary = "Create operation")
-	public ResponseEntity<Object> createOperation(
+	public ResponseEntity<String> createOperation(
 			@Parameter(description = "Operation to create", required = true, content = @Content(schema = @Schema(implementation = Operation.class))) @RequestBody String body) throws RmesException {
 		String id = operationsService.createOperation(body);
 		return ResponseEntity.status(HttpStatus.OK).body(id);
 	}
 
-	/**
-	 * PUBLISH
-	 * 
-	 * @param id
-	 * @return response
-	 */
 	@PreAuthorize("hasAnyRole(T(fr.insee.rmes.config.auth.roles.Roles).ADMIN " + ", T(fr.insee.rmes.config.auth.roles.Roles).SERIES_CONTRIBUTOR)")
 	@PutMapping(value = "/operation/validate/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@io.swagger.v3.oas.annotations.Operation(operationId = "setOperationValidation", summary = "Operation validation")
-	public ResponseEntity<Object> setOperationValidation(@PathVariable(Constants.ID) String id) throws RmesException {
+	public ResponseEntity<String> setOperationValidation(@PathVariable(Constants.ID) String id) throws RmesException {
 		operationsService.setOperationValidation(id);
 		return ResponseEntity.status(HttpStatus.OK).body(id);
 	}
