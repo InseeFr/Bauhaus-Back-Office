d744cfb8dd65cc0477256b4ef5c5bcd71002dc6d
Bibonne Fabrice <fabrice.bibonne@insee.fr>
2024-10-03 06:54:32 +0200

    test (cors : tests skeletons)

---
+++ src/test/java/fr/insee/rmes/integration/CorsConfigDefaultToAppHostPropertyForOriginTest.java
@@ -0,0 +1,23 @@
+package fr.insee.rmes.integration;
+
+import fr.insee.rmes.config.BaseConfigForMvcTests;
+import fr.insee.rmes.webservice.PublicResources;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = PublicResources.class,properties = {"fr.insee.rmes.bauhaus.cors.allowedOrigin=","fr.insee.rmes.bauhaus.appHost="})
+@Import(BaseConfigForMvcTests.class)
+class CorsConfigDefaultToAppHostPropertyForOriginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenPerformCorsRequest_shouldBeDenied(){
+
+    }
+
+}
