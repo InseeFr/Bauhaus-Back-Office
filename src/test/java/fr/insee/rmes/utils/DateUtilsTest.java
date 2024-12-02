19c1d64973247ac44425341776bfcb5e35f290b6
Emmanuel <demey.emmanuel@gmail.com>
2024-12-02 09:35:14 +0000

    fix: remove unused exception

--- src/test/java/fr/insee/rmes/utils/DateUtilsTest.java
+++ src/test/java/fr/insee/rmes/utils/DateUtilsTest.java
@@ -8,13 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DateUtilsTest {
 
 	@Test
-	void should_return_date_if_good_format() throws RmesException {
+	void should_return_date_if_good_format() {
 		String date = DateUtils.getDate("2018-12-17");
 		assertEquals("2018-12-17", date);
 	}
 
 	@Test
-	void should_return_date_if_bad_format() throws RmesException {
+	void should_return_date_if_bad_format() {
 		String date = DateUtils.getDate("12-17");
 		assertEquals("12-17", date);
 	}
