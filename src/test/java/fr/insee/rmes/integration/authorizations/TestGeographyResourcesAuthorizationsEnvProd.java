881078adbb2392dc036dee084b779376b1b23c92
Emmanuel <demey.emmanuel@gmail.com>
2024-12-13 11:29:29 +0000

    fix: remove sonar issues

--- src/test/java/fr/insee/rmes/integration/authorizations/TestGeographyResourcesAuthorizationsEnvProd.java
+++ src/test/java/fr/insee/rmes/integration/authorizations/TestGeographyResourcesAuthorizationsEnvProd.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Role;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.web.servlet.MockMvc;
