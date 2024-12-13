5a99c51aee718336ea7d2212f25fec49e783551c
Fabrice B <fabrice.bibonne@insee.fr>
2024-12-13 11:43:42 +0100

    fix (print debug message with miniofiles)

--- src/main/java/fr/insee/rmes/bauhaus_services/MinioFilesOperation.java
+++ src/main/java/fr/insee/rmes/bauhaus_services/MinioFilesOperation.java
@@ -1,8 +1,11 @@
 package fr.insee.rmes.bauhaus_services;
 
+import fr.insee.rmes.bauhaus_services.operations.documentations.documents.DocumentsUtils;
 import fr.insee.rmes.exceptions.RmesFileException;
 import io.minio.*;
 import io.minio.errors.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,10 +17,14 @@ import static java.util.Objects.requireNonNull;
 
 public record MinioFilesOperation(MinioClient minioClient, String bucketName, String directoryGestion, String directoryPublication) implements FilesOperations {
 
+    static final Logger logger = LoggerFactory.getLogger(MinioFilesOperation.class);
+
     @Override
     public InputStream read(String pathFile){
         String fileName= extractFileName(pathFile);
         String objectName = directoryGestion + "/" + fileName;
+
+        logger.debug("Reading file with name {} from path {} as object {} in bucket {}", fileName, pathFile, objectName, bucketName);
 
         try {
             return minioClient.getObject(GetObjectArgs.builder()
@@ -38,6 +45,7 @@ public record MinioFilesOperation(MinioClient minioClient, String bucketName, St
     @Override
     public boolean existsInStorage(String filename) {
         var objectName = extractFileName(requireNonNull(filename));
+        logger.debug("Check existence of file with name {} as object {} in bucket {}", filename, objectName, bucketName);
         try {
             return minioClient.statObject(StatObjectArgs.builder()
                     .bucket(bucketName)
@@ -52,6 +60,7 @@ public record MinioFilesOperation(MinioClient minioClient, String bucketName, St
     public void write(InputStream content, Path filePath) {
         String filename = filePath.getFileName().toString();
         String objectName = directoryGestion + "/" + filename;
+        logger.debug("Writing to file with name {} from path {} as object {} in bucket {}", filename, filePath, objectName, bucketName);
         try {
             minioClient.putObject(PutObjectArgs.builder()
                     .bucket(bucketName)
@@ -70,6 +79,8 @@ public record MinioFilesOperation(MinioClient minioClient, String bucketName, St
 
         String srcObject = directoryGestion + "/" + extractFileName(srcObjectName);
         String destObject = directoryPublication + "/" + extractFileName(srcObjectName);
+
+        logger.debug("Copy from source {} as object {} to destination {} as object {} in bucket {}", srcObjectName, srcObject, destObjectName, destObject, bucketName);
 
         try {
             CopySource source = CopySource.builder()
@@ -96,6 +107,9 @@ public record MinioFilesOperation(MinioClient minioClient, String bucketName, St
      @Override
     public void delete(Path absolutePath) {
         String objectName = absolutePath.getFileName().toString();
+
+        logger.debug("Delete file with path {} as object {} in bucket {}", absolutePath, objectName, bucketName);
+
         try {
             minioClient.removeObject(RemoveObjectArgs.builder()
                     .bucket(bucketName)
