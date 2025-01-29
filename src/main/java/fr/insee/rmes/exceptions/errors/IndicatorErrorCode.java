02dc97d58a00f36431c32c49a0c022df1ef36d8d
Emmanuel <demey.emmanuel@gmail.com>
2025-01-29 16:02:11 +0100

    fix: solve sonar issue

--- src/main/java/fr/insee/rmes/exceptions/errors/IndicatorErrorCode.java
+++ src/main/java/fr/insee/rmes/exceptions/errors/IndicatorErrorCode.java
@@ -2,6 +2,9 @@ package fr.insee.rmes.exceptions.errors;
 
 public class IndicatorErrorCode {
 
+    private IndicatorErrorCode() {
+    }
+
     public static final String EMPTY_WAS_GENERATED_BY = "406_OPERATION_INDICATOR_OPERATION_INDICATOR_EMPTY_WAS_GENERATED_BY";
     public static final String EXISTING_PREF_LABEL_LG1 = "406_OPERATION_INDICATOR_OPERATION_INDICATOR_EXISTING_PREF_LABEL_LG1";
     public static final String EXISTING_PREF_LABEL_LG2 = "406_OPERATION_INDICATOR_OPERATION_INDICATOR_EXISTING_PREF_LABEL_LG2";
