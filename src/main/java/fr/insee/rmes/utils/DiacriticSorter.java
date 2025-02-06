af7de20e781d16dfa16051fdeaa3e0ad117c6739
Fabrice B <fabrice.bibonne@insee.fr>
2025-02-06 07:23:12 +0100

    fix (DiacriticSorter NPE when a sort is null) null sort keyx are replaced by empty string

--- src/main/java/fr/insee/rmes/utils/DiacriticSorter.java
+++ src/main/java/fr/insee/rmes/utils/DiacriticSorter.java
@@ -19,12 +19,11 @@ public class DiacriticSorter {
      * @param targetArrayClass the class of the array elements
      * @param keyExtractor a function to extract the sorting key from each element
      * @param businessProcessor an optional processing function to modify the stream before sorting
-     * @param <S> the type of the sorting key (must be Comparable)
      * @param <T> the type of the elements in the array
      * @return a sorted list of objects
      * @throws RmesException if the JSON cannot be deserialized or an error occurs during processing
      */
-    private static <S extends Comparable<S>, T> List<T> sort(String jsonArray, Class<T[]> targetArrayClass, Function<T, S> keyExtractor, Optional<UnaryOperator<Stream<T>>> businessProcessor) throws RmesException {
+    private static <T> List<T> sort(String jsonArray, Class<T[]> targetArrayClass, Function<T, String> keyExtractor, Optional<UnaryOperator<Stream<T>>> businessProcessor) throws RmesException {
         var stream= Arrays.stream(Deserializer.deserializeJsonString(jsonArray, targetArrayClass));
         stream = businessProcessor.orElse(UnaryOperator.identity()).apply(stream);
         return stream.sorted(getComparator(keyExtractor)).toList();
@@ -39,18 +38,19 @@ public class DiacriticSorter {
         )).values().stream();
     }
 
-    public static <S extends Comparable<S>, T extends AppendableLabel<T>> List<T> sortGroupingByIdConcatenatingAltLabels(String jsonArray, Class<T[]> targetArrayClass, Function<T, S> keyExtractor) throws RmesException {
+    public static <T extends AppendableLabel<T>> List<T> sortGroupingByIdConcatenatingAltLabels(String jsonArray, Class<T[]> targetArrayClass, Function<T, String> keyExtractor) throws RmesException {
         return sort(jsonArray, targetArrayClass, keyExtractor, Optional.of(businessProcessor()));
     }
 
-    public static <S extends Comparable<S>, T> List<T> sort(String jsonArray, Class<T[]> targetArrayClass, Function<T, S> keyExtractor) throws RmesException {
+    public static <T> List<T> sort(String jsonArray, Class<T[]> targetArrayClass, Function<T, String> keyExtractor) throws RmesException {
         return sort(jsonArray, targetArrayClass, keyExtractor, Optional.empty());
     }
 
-    private static <S extends Comparable<S>, T> Comparator<T> getComparator(Function<T, S> projectToComparableField) {
+    private static <T> Comparator<T> getComparator(Function<T, String> projectToComparableField) {
         Collator collator = Collator.getInstance(Locale.FRENCH);
         collator.setStrength(Collator.PRIMARY);
-        return Comparator.comparing(projectToComparableField, collator);
+        UnaryOperator<String> nullSafer = s->s==null?"":s;
+        return Comparator.comparing(nullSafer.compose(projectToComparableField), collator);
     }
 
 }
