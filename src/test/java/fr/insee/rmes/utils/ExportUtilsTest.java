f52a4830a620d1432ca0ec4a443b5bd996ffc049
Fabrice B <fabrice.bibonne@insee.fr>
2025-03-04 12:02:16 +0100

    refactor (test whenExportAsInputStream_ShouldNotWriteOnDisk) use jfr to check if method write data on disk

--- src/test/java/fr/insee/rmes/utils/ExportUtilsTest.java
+++ src/test/java/fr/insee/rmes/utils/ExportUtilsTest.java
@@ -1,13 +1,13 @@
 package fr.insee.rmes.utils;
 
 import fr.insee.rmes.exceptions.RmesException;
-import org.assertj.core.util.Files;
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingStream;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.StandardWatchEventKinds;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static fr.insee.rmes.bauhaus_services.Constants.COLLECTION;
@@ -17,12 +17,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ExportUtilsTest {
 
     @Test
-    void whenExportAsInputStream_ShouldNotWriteOnDisk() throws IOException, RmesException {
+    void whenExportAsInputStream_ShouldNotWriteOnDisk() throws RmesException {
+
         ExportUtils exportUtils = new ExportUtils(10);
-        var watchService = FileSystems.getDefault().newWatchService();
-        var watchKey = Path.of(Files.temporaryFolderPath()).register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_CREATE);
-        exportUtils.exportAsInputStream("toto", Map.of(), XSL_FILE, XML_PATTERN_ODS, ZIP_ODS, COLLECTION, FilesUtils.ODS_EXTENSION);
-        assertThat(watchKey.pollEvents()).isEmpty();
-        watchService.close();
+        List<RecordedEvent> recordedEvents = new ArrayList<>();
+        InputStream zipStream;
+        try (var rs = new RecordingStream()) {
+            rs.enable("jdk.FileWrite").withoutThreshold().withStackTrace();
+            rs.onEvent("jdk.FileWrite", recordedEvents::add);
+            rs.startAsync();
+            zipStream = exportUtils.exportAsInputStream("toto", Map.of(), XSL_FILE, XML_PATTERN_ODS, ZIP_ODS, COLLECTION, FilesUtils.ODS_EXTENSION);
+            rs.stop();
+        }
+        assertThat(recordedEvents).isEmpty();
+        assertThat(zipStream).isNotEmpty();
     }
 }
