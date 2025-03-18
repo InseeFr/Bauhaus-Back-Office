36609378c72d69319b31783ca0ada9874492f2bc
SM <sofiane.maazi@insee.fr>
2025-03-18 08:11:47 +0100

    add test StructureComponentUtilsTest.java

---
+++ src/test/java/fr/insee/rmes/bauhaus_services/structures/utils/StructureComponentUtilsTest.java
@@ -0,0 +1,32 @@
+package fr.insee.rmes.bauhaus_services.structures.utils;
+
+import com.sun.jna.Structure;
+import fr.insee.rmes.bauhaus_services.Constants;
+import fr.insee.rmes.bauhaus_services.rdf_utils.RdfUtils;
+import fr.insee.rmes.persistance.ontologies.INSEE;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StructureComponentUtilsTest {
+    
+    @Mock
+    Structure structures;
+
+    @Test
+    void shouldReturnFormatComponent() {
+
+        JSONObject response = new JSONObject();
+        response.put("structures", structures);
+        response.put("codeList","myCodeList");
+        if (response.has(Constants.CODELIST)) {
+            response.put("range", RdfUtils.toString(INSEE.CODELIST));
+        }
+        String expected = "{\"range\":\"http://rdf.insee.fr/def/base#codeList\",\"codeList\":\"myCodeList\"}";
+
+        assertEquals(expected,response.toString());
+    }
+
+}
