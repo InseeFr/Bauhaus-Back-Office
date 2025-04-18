f8ff53dbd3ec667b2381e7906244bf6191ae86f9
SM <sofiane.maazi@insee.fr>
2025-04-18 12:06:16 +0200

    feature : method for NotesUtilsTest.java

---
+++ src/test/java/fr/insee/rmes/bauhaus_services/notes/NotesUtilsTest.java
@@ -0,0 +1,20 @@
+package fr.insee.rmes.bauhaus_services.notes;
+
+import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.model.concepts.Concept;
+import fr.insee.rmes.model.notes.VersionableNote;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesUtilsTest {
+
+    @Test
+    void shouldGetVersion() throws RmesException {
+        Concept concept = new Concept("id",true);
+        VersionableNote versionableNote = new VersionableNote();
+        String defaultBVersion = "defaultVersion";
+        NotesUtils notesUtils = new NotesUtils();
+        String Result= notesUtils.getVersion(concept,versionableNote,defaultBVersion);
+        assertEquals("1",Result);
+    }
+}
