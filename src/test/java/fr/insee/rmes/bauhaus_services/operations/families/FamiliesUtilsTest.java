56cbe753b53abf8c97d4a2856db09c87891ddab8
SM <sofiane.maazi@insee.fr>
2025-04-29 12:38:13 +0200

    feature : Sonar issues

--- src/test/java/fr/insee/rmes/bauhaus_services/operations/families/FamiliesUtilsTest.java
+++ src/test/java/fr/insee/rmes/bauhaus_services/operations/families/FamiliesUtilsTest.java
@@ -1,10 +1,6 @@
 package fr.insee.rmes.bauhaus_services.operations.families;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
-import fr.insee.rmes.exceptions.RmesBadRequestException;
 import fr.insee.rmes.exceptions.RmesException;
 import fr.insee.rmes.model.operations.Family;
 import org.eclipse.rdf4j.model.IRI;
@@ -17,8 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -27,53 +21,6 @@ class FamiliesUtilsTest {
 
     @Mock
     private RepositoryGestion repositoryGestion;
-
-    @Test
-    void shouldReturnAnExceptionWhenTitleIsNotPresentAtLeast() throws RmesException, JsonProcessingException {
-
-        String body= "{\n" +
-                "  \"prefLabelLg3\": \"Activité, production et chiffre d'affaires\",\n" +
-                "  \"prefLabelLg2\": \"Activity, production and turnover\",\n" +
-                "  \"series\": [\n" +
-                "    {\n" +
-                "      \"labelLg2\": \"Monthly branch surveys\",\n" +
-                "      \"labelLg1\": \"Enquêtes mensuelles de branche\",\n" +
-                "      \"id\": \"s1006\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"labelLg2\": \"Survey on the impact of the health crisis on the businesses organisation and activity\",\n" +
-                "      \"labelLg1\": \"Enquête sur l'impact de la crise sanitaire sur l'organisation et l'activité des entreprises\",\n" +
-                "      \"id\": \"s1044\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"labelLg2\": \"Monthly survey of large-scale food retail activities\",\n" +
-                "      \"labelLg1\": \"Enquête mensuelle sur l'activité des grandes surfaces alimentaires\",\n" +
-                "      \"id\": \"s1222\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"labelLg2\": \"Computation of activity and turnover indices\",\n" +
-                "      \"labelLg1\": \"Élaboration des indicateurs d'activité et de chiffre d'affaires\",\n" +
-                "      \"id\": \"s1284\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"labelLg2\": \"Test 4\",\n" +
-                "      \"labelLg1\": \"Test 4\",\n" +
-                "      \"id\": \"s2284\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"modified\": \"2024-07-17T11:16:55.419150048\",\n" +
-                "  \"id\": \"s82\",\n" +
-                "  \"validationState\": \"Modified\"\n" +
-                "}";
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Family family = mapper.readValue(body,Family.class);
-
-        RmesException exception = assertThrows(RmesBadRequestException.class, () -> FamiliesUtils.verifyBodyToCreateFamily(family));
-        assertThat(exception.getDetails()).contains("Required title not entered by user.");
-    }
-
 
     @Test
     void shouldAddAbstractPropertyWithNewSyntaxIfFeatureFlagTrue() throws RmesException {
