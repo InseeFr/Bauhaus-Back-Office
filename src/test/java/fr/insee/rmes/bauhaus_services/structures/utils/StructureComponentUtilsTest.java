0d8e8334e57d20fe72d36f54f509501283478799
SM <sofiane.maazi@insee.fr>
2025-04-30 14:37:34 +0200

    feature : methods for StructureComponentUtilsTest.java

--- src/test/java/fr/insee/rmes/bauhaus_services/structures/utils/StructureComponentUtilsTest.java
+++ src/test/java/fr/insee/rmes/bauhaus_services/structures/utils/StructureComponentUtilsTest.java
@@ -8,28 +8,46 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StructureComponentUtilsTest {
 
+
+    StructureComponentUtils structureComponentUtils = new StructureComponentUtils();
+
     @Test
     void shouldThrowRmesExceptionWhenUpdateComponent() {
-        StructureComponentUtils structureComponentUtils = new StructureComponentUtils();
         RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("componentId","{\"id\":\"idExample\",\"creator\":\"creatorExample\"}"));
         assertThat(exception.getDetails()).contains("The id of the component should be the same as the one defined in the request");
     }
 
     @Test
     void shouldThrowRmesExceptionWhenValidateComponentWithoutIdentifiant() {
-        StructureComponentUtils structureComponentUtils = new StructureComponentUtils();
         RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("idExample","{\"id\":\"idExample\",\"creator\":\"creatorExample\"}"));
         assertThat(exception.getDetails()).contains("The property identifiant is required");
     }
 
     @Test
     void shouldThrowRmesExceptionWhenValidateComponentWithoutLabelLg1() {
-        StructureComponentUtils structureComponentUtils = new StructureComponentUtils();
         RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("idExample","{\"id\":\"idExample\",\"identifiant\":\"identifiantExample\",\"creator\":\"creatorExample\"}"));
         assertThat(exception.getDetails()).contains("{\"message\":\"The property labelLg1 is required\"}");
     }
-    
+
+    @Test
+    void shouldThrowRmesExceptionWhenValidateComponentWithoutLabelLg2() {
+        RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("idExample","{\"id\":\"idExample\",\"identifiant\":\"identifiantExample\",\"labelLg1\":\"labelLg1Example\"}"));
+        assertThat(exception.getDetails()).contains("{\"message\":\"The property labelLg2 is required\"}");
     }
+
+    @Test
+    void shouldThrowRmesExceptionWhenValidateComponentWithoutType() {
+        RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("idExample","{\"id\":\"idExample\",\"identifiant\":\"identifiantExample\",\"labelLg1\":\"labelLg1Example\",\"labelLg2\":\"labelLg2Example\"}"));
+        assertThat(exception.getDetails()).contains("{\"message\":\"The property type is required\"}");
+    }
+
+    @Test
+    void shouldThrowRmesExceptionWhenValidateComponentWithInvalidateType() {
+        RmesException exception = assertThrows(RmesBadRequestException.class, () ->  structureComponentUtils.updateComponent("idExample","{\"id\":\"idExample\",\"identifiant\":\"identifiantExample\",\"labelLg1\":\"labelLg1Example\",\"labelLg2\":\"labelLg2Example\",\"type\":\"typeExample\"}"));
+        assertThat(exception.getDetails()).contains("{\"message\":\"The property type is not valid\"}");
+    }
+
+}
 
 
 
