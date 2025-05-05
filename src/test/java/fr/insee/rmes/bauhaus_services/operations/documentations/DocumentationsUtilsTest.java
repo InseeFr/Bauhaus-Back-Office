c7aa84159494b9a22a11da0184cbcd8f3205d267
SM <sofiane.maazi@insee.fr>
2025-05-05 15:35:25 +0200

    feature : methods for DocumentationsUtilsTest.java

--- src/test/java/fr/insee/rmes/bauhaus_services/operations/documentations/DocumentationsUtilsTest.java
+++ src/test/java/fr/insee/rmes/bauhaus_services/operations/documentations/DocumentationsUtilsTest.java
@@ -2,25 +2,41 @@ package fr.insee.rmes.bauhaus_services.operations.documentations;
 
 import fr.insee.rmes.bauhaus_services.Constants;
 import fr.insee.rmes.bauhaus_services.operations.ParentUtils;
-import fr.insee.rmes.exceptions.RmesBadRequestException;
-import fr.insee.rmes.exceptions.RmesException;
-import fr.insee.rmes.exceptions.RmesNotAcceptableException;
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
+import fr.insee.rmes.config.auth.security.restrictions.StampsRestrictionsService;
+import fr.insee.rmes.exceptions.*;
 import fr.insee.rmes.model.ValidationStatus;
 import fr.insee.rmes.model.operations.documentations.Documentation;
 import fr.insee.rmes.model.operations.documentations.DocumentationRubric;
 import fr.insee.rmes.model.operations.documentations.MAS;
 import fr.insee.rmes.model.operations.documentations.MSD;
+import fr.insee.rmes.persistance.sparql_queries.operations.documentations.DocumentationsQueries;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(properties = { "fr.insee.rmes.bauhaus.lg1=fr", "fr.insee.rmes.bauhaus.lg2=en"})
 class DocumentationsUtilsTest {
+
+	@MockitoBean
+	RepositoryGestion repoGestion;
+
+	@MockitoBean
+	ParentUtils parentUtils;
+
+	@MockitoBean
+	StampsRestrictionsService stampsRestrictionsService;
 
 	@InjectMocks
 	@Spy
@@ -36,6 +52,43 @@ class DocumentationsUtilsTest {
 	public void init() {
 		MockitoAnnotations.openMocks(this);
 	}
+
+	@Test
+	void shouldThrowRmesNotFoundExceptionWhenGetDocumentationByIdSims() throws RmesException {
+		String idSims ="2025";
+		when(repoGestion.getResponseAsObject(DocumentationsQueries.getDocumentationTitleQuery(idSims))).thenReturn(new JSONObject());
+		RmesException exception = assertThrows(RmesNotFoundException.class, () -> documentationsUtils.getDocumentationByIdSims(idSims));
+		assertTrue(exception.getDetails().contains("Documentation not found"));
+	}
+
+	@Test
+	void shouldThrowRmesNotAcceptableExceptionWhenDeleteMetadataReport() throws RmesException {
+		String id ="2025";
+		String[] days = {"yesterday","today","tomorrow"};
+		when(parentUtils.getDocumentationTargetTypeAndId(id)).thenReturn(days);
+		RmesException exception = assertThrows(RmesNotAcceptableException.class, () -> documentationsUtils.deleteMetadataReport(id));
+		assertTrue(exception.getDetails().contains("Only a sims that documents a series can be deleted"));
+	}
+
+	@Test
+	void shouldThrowRmesUnauthorizedExceptionWhenDeleteMetadataReport() throws RmesException {
+		String id ="2025";
+		String[] examples = {Constants.SERIES_UP,"today","tomorrow"};
+		when(parentUtils.getDocumentationTargetTypeAndId(id)).thenReturn(examples);
+		RmesException exception = assertThrows(RmesUnauthorizedException.class, () -> documentationsUtils.deleteMetadataReport(id));
+		assertTrue(exception.getDetails().contains("Only an admin or a manager can delete a sims."));
+	}
+
+
+	@Test
+	void shouldThrowRmesUnauthorizedExceptionWhenDeleteMetadataReportf() throws RmesException {
+		String id ="2025";
+		String[] examples = {Constants.SERIES_UP,"today","tomorrow"};
+		when(parentUtils.getDocumentationTargetTypeAndId(id)).thenReturn(examples);
+		RmesException exception = assertThrows(RmesUnauthorizedException.class, () -> documentationsUtils.deleteMetadataReport(id));
+		assertTrue(exception.getDetails().contains("Only an admin or a manager can delete a sims."));
+	}
+
 
 	@Test
 	void shouldThrowExceptionIfParentTargetIsUnpublished() throws RmesException {
@@ -111,5 +164,4 @@ class DocumentationsUtilsTest {
 		boolean verifyVariableIdParent = Objects.equals(mds.getMasList().getLast().getIdParent(),jsonMsdRubricTwo.getString("idParent"));
 		assertTrue(verifyVariableIdMas && verifyVariableMasLabelLg1 && verifyVariableIdParent );
 	}
-
 }
