ae13e186b55de5fc053613cfdb5d96a6833b4d68
Hugo Bouttes <hugo.bouttes@insee.fr>
2025-05-15 15:05:30 +0200

    test test

--- src/main/java/fr/insee/rmes/utils/XsltUtils.java
+++ src/main/java/fr/insee/rmes/utils/XsltUtils.java
@@ -19,6 +19,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -38,21 +39,32 @@ public class XsltUtils {
 		StreamSource xsrc = new StreamSource(xslFileIS);
 		Transformer xsltTransformer = XMLUtils.getTransformerFactory().newTransformer(xsrc);
 
-		xmlContent.forEach((paramName, xmlData) -> {
-			try {
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-				dbf.setNamespaceAware(true);
-				DocumentBuilder builder = dbf.newDocumentBuilder();
-				Document doc = builder.parse(new ByteArrayInputStream(xmlData.getBytes(StandardCharsets.UTF_8)));
+		// Liste des paramètres XSLT attendus
+		List<String> expectedParams = List.of(
+				"series", "operation", "indicator", "sims",
+				"organizations", "codeLists", "msd", "concepts",
+				"collections", "parameters"
+		);
 
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+
+		try {
+			DocumentBuilder builder = dbf.newDocumentBuilder();
+
+			for (String paramName : expectedParams) {
+				String xmlData = xmlContent.getOrDefault(paramName, "<" + paramName + "/>");
+				Document doc = builder.parse(new ByteArrayInputStream(xmlData.getBytes(StandardCharsets.UTF_8)));
 				xsltTransformer.setParameter(paramName, new DOMSource(doc.getDocumentElement()));
-			} catch (Exception e) {
-				throw new RuntimeException("Error setting XML parameter " + paramName, e);
 			}
-		});
+
+		} catch (Exception e) {
+			throw new RuntimeException("Error setting XSLT parameters", e);
+		}
 
 		xsltTransformer.transform(new StreamSource(odtFileIS), new StreamResult(outputStream));
 	}
+
 
 
 
