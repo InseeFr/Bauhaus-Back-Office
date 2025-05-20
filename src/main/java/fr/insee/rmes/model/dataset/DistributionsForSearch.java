3ab2267992d95e22ba28fdd53ef141a4c080e5e6
Pierre Vasseur <pierre.vasseur@insee.fr>
2025-05-20 16:32:35 +0200

    feature: add advanced search for distributions

---
+++ src/main/java/fr/insee/rmes/model/dataset/DistributionsForSearch.java
@@ -0,0 +1,17 @@
+package fr.insee.rmes.model.dataset;
+
+public record DistributionsForSearch(
+        String distributionId,
+        String distributionLabelLg1,
+        String distributionValidationStatus,
+        String distributionCreated,
+        String distributionUpdated,
+        String id,
+        String labelLg1,
+        String creator,
+        String disseminationStatus,
+        String validationStatus,
+        String wasGeneratedIRIs,
+        String created,
+        String updated
+) {}
