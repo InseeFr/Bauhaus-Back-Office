6ec8dc16de096d6098edaa7424c0c18b5336aacb
SM <sofiane.maazi@insee.fr>
2025-05-22 11:56:56 +0200

    feature : methods for CollectionExportBuilderTest.java (#905)
    * feature : methods for CollectionExportBuilderTest.java
    
    * feature : methods for ConceptsUtilsTest.java
    
    * feature : methods for DocumentsUtilsTest.java
    
    * feature : Sonar issues
    
    * feature : add methods for DocumentationRubricTest.java
    
    * feature : Sonar issues
    
    * feature : methods for FamOpeSerIndUtilsTest.java
    
    * feature : methods for QueryUtilsTest.java
    
    * feature : methods for RdfUtilsTest.java
    
    * feature : Sonar Issues
    
    * feature : add methods for RepositoryUtilsTest.java
    
    * feature : methods for AuthTypeTest.java
    
    * feature : add methods for XMLUtilsTest.java
    
    * feature : Sonar issues
    
    * feature : methods for DeserializerTest.java
    
    * feature : add methods JSONComparatorTest.java
    
    * feature : method for NotesUtilsTest.java
    
    * feature : Sonar issues
    
    * feature : methods for KeycloakServicesTest.java
    
    * feature : methods for CommonSecurityConfigurationTest.java
    
    * feature : methods for RmesExceptionHandlerTest.java
    
    * feature : Sonar issues
    
    * feature : methods for RmesExceptionHandlerTest.java
    
    * feature : methods for XhtmlToMarkdownUtilsTest.java
    
    * feature : methods for ClasspathUriResolverTest.java
    
    * feature : improve ClasspathUriResolverTest.java
    
    * feature : methods for MetadataStructureDefUtilsTest.java
    
    * feature : methods for DocumentationsUtilsTest.java
    
    * feature : methods for CodeListServiceImplTest.java
    
    * feature : Sonar issue
    
    * feature : methods for XsltUtilsTest.java
    
    * feature : methods for CollectionsUtilsTest.java
    
    * feature : methods for DocumentsImplTest.java
    
    * feature : Sonar Issues
    
    * feature : method for FamiliesUtilsTest.java
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : sonar issues
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : sonar issues
    
    * feature : Sonar issues
    
    * feature : methods for RepositoryUtilsTest.java
    
    * feature : method for StructureComponentUtilsTest.java
    
    * feature : method for StructureComponentUtils.java
    
    * feature : methods for StructureComponentUtilsTest.java
    
    * feature : methods for StructureComponentUtilsTest.java
    
    * feature : methods for StructureComponentUtilsTest.java
    
    * feature : methods for StructureUtilsTest.java
    
    * feature : methods for ClassificationItemServiceImplTest.java
    
    * feature : improve methods in ClassificationItemServiceImplTest.java
    
    * feature : methods for ClassificationPublicationTest.java
    
    * feature : improve methods
    
    * feature : methods for ClassificationsImplTest.java
    
    * feature : methods for ParentUtilsTest.java
    
    * feature : methods for MetadataStructureDefUtilsTest.java
    
    * feature : methods for ConceptsUtilsTest.java
    
    * feature : methods for DocumentationsUtilsTest.java
    
    * feature : Sonar issues
    
    * feature : Sonar issues
    
    * feature : Sonar Issues
    
    * feature : corrected methods for DocumentationsUtilsTest.java

---
+++ src/test/java/fr/insee/rmes/bauhaus_services/classifications/item/ClassificationItemServiceImplTest.java
@@ -0,0 +1,58 @@
+package fr.insee.rmes.bauhaus_services.classifications.item;
+
+import fr.insee.rmes.bauhaus_services.rdf_utils.RepositoryGestion;
+import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.persistance.sparql_queries.classifications.ItemsQueries;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(properties = { "fr.insee.rmes.bauhaus.lg1=fr", "fr.insee.rmes.bauhaus.lg2=en"})
+class ClassificationItemServiceImplTest {
+
+    @InjectMocks
+    ClassificationItemServiceImpl classificationItemServiceImpl = new ClassificationItemServiceImpl();
+
+    @MockitoBean
+    RepositoryGestion repoGestion;
+
+    String classificationId = "classificationID";
+    String itemId = "itemId";
+    String body = "fake body for Json Object";
+    JSONObject item = new JSONObject().put("A", "letterA").put("B", "letterB");
+
+
+    @Test
+    void shouldGetClassificationItemsWhenAltLabelNotNull() throws RmesException {
+        JSONArray altLabels = new JSONArray().put(new JSONObject().put("C", "letterC").put("D", "letterD"));
+        when(repoGestion.getResponseAsObject(ItemsQueries.itemQuery(classificationId, itemId))).thenReturn(item);
+        when(repoGestion.getResponseAsArray(ItemsQueries.itemAltQuery(classificationId, itemId))).thenReturn(altLabels);
+        String actual = classificationItemServiceImpl.getClassificationItem(classificationId, itemId);
+        String expected = "{\"A\":\"letterA\",\"B\":\"letterB\",\"altLabels\":[{\"C\":\"letterC\",\"D\":\"letterD\"}]}";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGetClassificationItemsWhenAltLabelIsNull() throws RmesException {
+        when(repoGestion.getResponseAsObject(ItemsQueries.itemQuery(classificationId, itemId))).thenReturn(item);
+        when(repoGestion.getResponseAsArray(ItemsQueries.itemAltQuery(classificationId, itemId))).thenReturn(new JSONArray());
+        String actual = classificationItemServiceImpl.getClassificationItem(classificationId, itemId);
+        Assertions.assertEquals(item.toString(), actual);
+    }
+
+    @Test
+    void shouldThrowRmesExceptionWhenUpdateClassificationItem(){
+        RmesException exception = assertThrows(RmesException.class, () -> classificationItemServiceImpl.updateClassificationItem(classificationId,itemId,body));
+        Assertions.assertTrue(exception.getDetails().contains("{\"details\":\"Can't read request body\""));
+    }
+
+}
