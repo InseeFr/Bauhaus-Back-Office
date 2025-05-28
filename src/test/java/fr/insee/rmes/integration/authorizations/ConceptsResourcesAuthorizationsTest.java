c589ffa0ab0ce0f1f6a1afd1b407b518781ed199
SM <sofiane.maazi@insee.fr>
2025-05-28 15:51:34 +0200

    feature : sonar issues

--- src/test/java/fr/insee/rmes/integration/authorizations/ConceptsResourcesAuthorizationsTest.java
+++ src/test/java/fr/insee/rmes/integration/authorizations/ConceptsResourcesAuthorizationsTest.java
@@ -50,7 +50,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         UserProviderFromSecurityContext.class,
         BauhausMethodSecurityExpressionHandler.class})
 
-class TestConceptsResourcesAuthorizationsEnvProd {
+class ConceptsResourcesAuthorizationsTest {
 
     @Autowired
     private MockMvc mvc;
