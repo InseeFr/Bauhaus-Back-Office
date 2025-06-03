520b6982b64505ea2073fd6dce18804aab29d3b2
SM <sofiane.maazi@insee.fr>
2025-06-03 13:30:47 +0200

    feature : methods for DatasetResourcesTest.java

---
+++ src/test/java/fr/insee/rmes/webservice/datasets/DatasetResourcesTest.java
@@ -0,0 +1,32 @@
+package fr.insee.rmes.webservice.datasets;
+
+import fr.insee.rmes.bauhaus_services.ConceptsService;
+import fr.insee.rmes.bauhaus_services.datasets.DatasetService;
+import fr.insee.rmes.exceptions.RmesException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import static org.mockito.Mockito.doNothing;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(properties = { "fr.insee.rmes.bauhaus.lg1=fr", "fr.insee.rmes.bauhaus.lg2=en"})
+class DatasetResourcesTest {
+
+    @MockitoBean
+    ConceptsService conceptsService;
+
+    @MockitoBean
+    DatasetService datasetService;
+
+    @Test
+    void shouldReturnResponseWhenDeleteDataset() throws RmesException {
+        doNothing().when(conceptsService).deleteConcept("id mocked");
+        DatasetResources datasetResources= new DatasetResources(datasetService);
+        String actual = datasetResources.deleteDataset("id mocked").toString();
+        Assertions.assertEquals("<200 OK OK,[]>",actual);
+    }
+
+}
