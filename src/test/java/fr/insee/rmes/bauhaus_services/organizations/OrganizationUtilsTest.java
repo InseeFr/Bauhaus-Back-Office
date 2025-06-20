9dbe2b93ab4a7fe9861d24ec6034971f0410e5ad
SM <sofiane.maazi@insee.fr>
2025-06-20 12:11:11 +0200

    feature : Sonar issue and method for OrganizationUtilsTest.java

---
+++ src/test/java/fr/insee/rmes/bauhaus_services/organizations/OrganizationUtilsTest.java
@@ -0,0 +1,30 @@
+package fr.insee.rmes.bauhaus_services.organizations;
+
+import fr.insee.rmes.bauhaus_services.operations.famopeserind_utils.FamOpeSerIndUtils;
+import fr.insee.rmes.config.swagger.model.IdLabelTwoLangs;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OrganizationUtilsTest {
+
+    @InjectMocks
+    OrganizationUtils organizationUtils;
+
+    @Mock
+    FamOpeSerIndUtils famOpeSerUtils;
+
+    @Test
+    void shouldBuildOrganizationFromJson()  {
+        JSONObject jsonObject = new JSONObject().put("color","blue");
+        IdLabelTwoLangs idLabelTwoLangs = new IdLabelTwoLangs("id","label1","label2");
+        when(famOpeSerUtils.buildIdLabelTwoLangsFromJson(jsonObject)).thenReturn(idLabelTwoLangs);
+        assertNotNull(organizationUtils.buildOrganizationFromJson(jsonObject));
+    }
+}
