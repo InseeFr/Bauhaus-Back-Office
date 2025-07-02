b34230413a6b892df44e884cb2c09c8de1b71c81
SM <sofiane.maazi@insee.fr>
2025-07-02 20:51:42 +0200

    Augmenter la couverture de tests de Bauhaus-Back-office (#933)
    * feature : methods for CodeListItemTest.java
    
    * feature : methods for DetailedCodeListTest.java
    
    * feature : methods for  MetadataStructureDefUtilsTest.java
    
    * feature : methods for OperationsImplTest.java
    
    * feature : methods for OrganizationsServiceImplTest.java
    
    * feature : Sonar issue and method for OrganizationUtilsTest.java
    
    * feature : methods for MutualizedComponentTest.java and ComponentDefinitionTest.java
    
    * feature : methods for StructureTest.java
    
    * feature : sonar issues
    
    * feature : methods for ValidationStatusTest.java
    
    * feature : method for DefaultStampCheckerTest .java
    
    * feature : method for UnknownApplicationExceptionTest.java
    
    * methods for UnknownPrivilegeExceptionTest .java
    
    * feature : method for UnknownRoleExceptionTest.java
    
    * feature : method for PublicResourcesTest.java
    
    * feature : Sonar issues

---
+++ src/test/java/fr/insee/rmes/model/structures/MutualizedComponentTest.java
@@ -0,0 +1,105 @@
+package fr.insee.rmes.model.structures;
+
+import fr.insee.rmes.exceptions.RmesException;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MutualizedComponentTest {
+
+    @Test
+    void shouldReturnMutualizedComponentAttributes() throws RmesException {
+
+        MutualizedComponent mutualizedComponent = new MutualizedComponent();
+
+        List<String> attributes = List.of("mockedIdentifiant",
+                "mockedIdentifiant",
+                "mockedLabelLg1",
+                "mockedLabelLg2",
+                "mockedAltLabelLg1",
+                "mockedAltLabelLg2",
+                "mockedDescriptionLg1",
+                "mockedDescriptionLg2",
+                "mockedType",
+                "mockedConcept",
+                "mockedCodeList",
+                "mockedFullCodeListValue",
+                "mockedRange",
+                "mockedCreated",
+                "mockedUpdated",
+                "mockedCreator",
+                "mockedDisseminationStatus",
+                "mockedMinLength",
+                "mockedMaxLength",
+                "mockedMinInclusive",
+                "mockedMaxInclusive",
+                "mockedPattern"
+                );
+
+        mutualizedComponent.setIdentifiant(attributes.getFirst());
+        mutualizedComponent.setId(attributes.get(1));
+        mutualizedComponent.setLabelLg1(attributes.get(2));
+        mutualizedComponent.setLabelLg2(attributes.get(3));
+        mutualizedComponent.setAltLabelLg1(attributes.get(4));
+        mutualizedComponent.setAltLabelLg2(attributes.get(5));
+        mutualizedComponent.setDescriptionLg1(attributes.get(6));
+        mutualizedComponent.setDescriptionLg2(attributes.get(7));
+        mutualizedComponent.setType(attributes.get(8));
+        mutualizedComponent.setConcept(attributes.get(9));
+        mutualizedComponent.setCodeList(attributes.get(10));
+        mutualizedComponent.setFullCodeListValue(attributes.get(11));
+        mutualizedComponent.setRange(attributes.get(12));
+        mutualizedComponent.setCreated(attributes.get(13));
+        mutualizedComponent.setUpdated(attributes.get(14));
+        mutualizedComponent.setCreator(attributes.get(15));
+        mutualizedComponent.setDisseminationStatus(attributes.get(16));
+        mutualizedComponent.setMinLength(attributes.get(17));
+        mutualizedComponent.setMaxLength(attributes.get(18));
+        mutualizedComponent.setMinInclusive(attributes.get(19));
+        mutualizedComponent.setMaxInclusive(attributes.get(20));
+        mutualizedComponent.setPattern(attributes.get(21));
+
+        List<String> actualAttributes= List.of(
+                mutualizedComponent.getIdentifiant(),
+                mutualizedComponent.getId(),
+                mutualizedComponent.getLabelLg1(),
+                mutualizedComponent.getLabelLg2(),
+                mutualizedComponent.getAltLabelLg1(),
+                mutualizedComponent.getAltLabelLg2(),
+                mutualizedComponent.getDescriptionLg1(),
+                mutualizedComponent.getDescriptionLg2(),
+                mutualizedComponent.getType(),
+                mutualizedComponent.getConcept(),
+                mutualizedComponent.getCodeList(),
+                mutualizedComponent.getFullCodeListValue(),
+                mutualizedComponent.getRange(),
+                mutualizedComponent.getCreated(),
+                mutualizedComponent.getUpdated(),
+                mutualizedComponent.getCreator(),
+                mutualizedComponent.getDisseminationStatus(),
+                mutualizedComponent.getMinLength(),
+                mutualizedComponent.getMaxLength(),
+                mutualizedComponent.getMinInclusive(),
+                mutualizedComponent.getMaxInclusive(),
+                mutualizedComponent.getPattern()
+        );
+
+        boolean stringParameters = attributes.equals(actualAttributes);
+
+        Structure[] structureArray= new Structure[]{new Structure(), new Structure()};
+        mutualizedComponent.setStructures(structureArray);
+
+        boolean structureParameters = Arrays.equals(structureArray, mutualizedComponent.getStructures());
+
+        List<String> contributors = new ArrayList<>();
+        contributors.add("firstContributor");
+        contributors.add("secondContributor");
+        mutualizedComponent.setContributor(contributors);
+
+        boolean stringArrayParameters = contributors.equals(mutualizedComponent.getContributor());
+
+        assertTrue(stringParameters && structureParameters && stringArrayParameters);
+    }
+}
---
+++ src/test/java/fr/insee/rmes/model/structures/StructureTest.java
@@ -0,0 +1,69 @@
+package fr.insee.rmes.model.structures;
+
+import fr.insee.rmes.exceptions.RmesException;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Objects;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StructureTest {
+
+    @Test
+    void shouldReturnStructureAttributes() throws RmesException {
+
+        Structure structure = new Structure();
+
+        Structure otherStructure = new Structure("mockedID");
+
+        List<String> attributes = List.of("mockedID",
+                "mockedLabelLg1",
+                "mockedLabelLg2",
+                "mockedDescriptionLg1",
+                "mockedDescriptionLg2",
+                "mockedCreated",
+                "mockedUpdated",
+                "mockedCreator",
+                "mockedDisseminationStatus",
+                "mockedIdentifiant");
+
+        structure.setId(attributes.getFirst());
+        structure.setLabelLg1(attributes.get(1));
+        structure.setLabelLg2(attributes.get(2));
+        structure.setDescriptionLg1(attributes.get(3));
+        structure.setDescriptionLg2(attributes.get(4));
+        structure.setCreated(attributes.get(5));
+        structure.setUpdated(attributes.get(6));
+        structure.setCreator(attributes.get(7));
+        structure.setDisseminationStatus(attributes.get(8));
+        structure.setIdentifiant(attributes.get(9));
+
+        List<String> actualAttributes = List.of(
+                structure.getId(),
+                structure.getLabelLg1(),
+                structure.getLabelLg2(),
+                structure.getDescriptionLg1(),
+                structure.getDescriptionLg2(),
+                structure.getCreated(),
+                structure.getUpdated(),
+                structure.getCreator(),
+                structure.getDisseminationStatus(),
+                structure.getIdentifiant());
+
+        boolean stringParameters = attributes.equals(actualAttributes);
+
+        List<String> stringArray= List.of("mockedString","mockedString");
+        structure.setContributor(stringArray);
+
+        boolean stringListParameters =stringArray.equals(structure.getContributor());
+
+        List<ComponentDefinition> componentDefinitionArray= List.of(new ComponentDefinition(),new ComponentDefinition());
+        structure.setComponentDefinitions(componentDefinitionArray);
+
+        boolean componentDefinitionListParameters =componentDefinitionArray.equals(structure.getComponentDefinitions());
+
+        boolean otherStructureAttribute= Objects.equals(otherStructure.getId(), "mockedID");
+
+        assertTrue(stringParameters && stringListParameters && componentDefinitionListParameters && otherStructureAttribute);
+
+    }
+}
