37c0577afd83703004b004f503813bf11fcaf387
SM <sofiane.maazi@insee.fr>
2025-07-23 14:42:06 +0200

    feature : methods for AcceptTest.java

---
+++ src/test/java/fr/insee/rmes/config/swagger/model/AcceptTest.java
@@ -0,0 +1,34 @@
+package fr.insee.rmes.config.swagger.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AcceptTest {
+
+    @Test
+    void shouldReturnAnyErrorWhenGetMediaType(){
+         String acceptJson = Accept.JSON.getMediaType();
+         String acceptXml = Accept.XML.getMediaType();
+         String expectedJson = "application/json";
+         String expectedXml = "application/xml";
+         assertTrue(expectedJson.equals(acceptJson) && expectedXml.equals(acceptXml));
+    }
+
+    @Test
+    void shouldReturnErrorWhenFromMediaType(){
+        String string = "mockedFakeAccept";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Accept.fromMediaType(string));
+        assertTrue(exception.getMessage().contains("Unsupported media type"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "application/json", "application/xml" })
+    void shouldNotReturnErrorWhenFromMediaType(String string){
+        Accept acceptActual = Accept.fromMediaType(string);
+        Accept  acceptJson = Accept.JSON;
+        Accept  acceptXml = Accept.XML;
+        assertTrue(acceptJson==acceptActual || acceptXml==acceptActual );
+    }
+}
