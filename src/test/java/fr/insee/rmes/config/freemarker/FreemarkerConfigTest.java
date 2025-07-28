de7e4a1b716bea5fd9dcf348c61750bd94e98842
SM <sofiane.maazi@insee.fr>
2025-07-28 15:28:46 +0200

    feat: sonar issue

--- src/test/java/fr/insee/rmes/config/freemarker/FreemarkerConfigTest.java
+++ src/test/java/fr/insee/rmes/config/freemarker/FreemarkerConfigTest.java
@@ -1,17 +1,15 @@
 package fr.insee.rmes.config.freemarker;
 
-import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreemarkerConfigTest {
 
     @Test
-    public void shouldInitFreemarkerConfig(){
-        StopWatch watch = new StopWatch();
-        watch.start();
+    void shouldInitFreemarkerConfig(){
+        var begin= System.nanoTime();
         FreemarkerConfig.init();
-        watch.stop();
-        assertTrue(watch.getTime()<=500);
+        var end = System. nanoTime();
+        assertTrue(end-begin<=1000000000);
     }
 }
