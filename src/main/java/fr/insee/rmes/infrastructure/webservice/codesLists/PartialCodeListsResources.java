ce0ecf5478d4852e693839c679344442913cffb6
Emmanuel <demey.emmanuel@gmail.com>
2025-08-11 14:40:34 +0100

    test: Init ArchUnit tests

---
+++ src/main/java/fr/insee/rmes/infrastructure/webservice/codesLists/PartialCodeListsResources.java
@@ -0,0 +1,141 @@
+package fr.insee.rmes.infrastructure.webservice.codesLists;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import fr.insee.rmes.bauhaus_services.CodeListService;
+import fr.insee.rmes.bauhaus_services.Constants;
+import fr.insee.rmes.config.swagger.model.code_list.CodeLabelList;
+import fr.insee.rmes.config.swagger.model.code_list.CodeList;
+import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.model.codeslists.PartialCodesList;
+import fr.insee.rmes.rbac.HasAccess;
+import fr.insee.rmes.rbac.RBAC;
+import fr.insee.rmes.infrastructure.webservice.GenericResources;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/codeList/partial")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Partial Code lists", description = "Partial Code list API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Success"),
+        @ApiResponse(responseCode = "204", description = "No Content"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized"),
+        @ApiResponse(responseCode = "403", description = "Forbidden"),
+        @ApiResponse(responseCode = "404", description = "Not found"),
+        @ApiResponse(responseCode = "406", description = "Not Acceptable"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")})
+public class PartialCodeListsResources extends GenericResources {
+
+    @Autowired
+    CodeListService codeListService;
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.READ)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Partial List of codes",
+            responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = CodeList.class)))})
+    public List<PartialCodesList> getAllPartialCodesLists() throws JsonProcessingException, RmesException {
+        return codeListService.getAllCodesLists(true);
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.READ)
+    @GetMapping(value = "/{notation}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get a partial list of code",
+            responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = CodeList.class)))})
+    public ResponseEntity<Object> getDetailedPartialCodesListByNotation (@PathVariable("notation") String notation) throws RmesException {
+        String body = codeListService.getDetailedPartialCodesList(notation);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.READ)
+    @GetMapping(value = "/parent/{parentCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get partials by Parent IRI",
+            responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = CodeLabelList.class)))})
+    public ResponseEntity<Object> getPartialsByParent(@PathVariable("parentCode") String parentIri) {
+        try {
+            String codesLists = codeListService.getPartialCodeListByParent(parentIri);
+            return ResponseEntity.status(HttpStatus.OK).body(codesLists);
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.PUBLISH)
+    @PutMapping("/{id}/validate")
+    @io.swagger.v3.oas.annotations.Operation(summary = "Publish a partial codelist")
+    public ResponseEntity<Object> publishPartialCodeList(
+            @PathVariable(Constants.ID) String id) {
+        try {
+            codeListService.publishCodeList(id, true);
+            return ResponseEntity.status(HttpStatus.OK).body(id);
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.READ)
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Return all lists for Advanced Search",
+            responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = CodeList.class)))})
+    public ResponseEntity<Object> getDetailedPartialCodesLisForSearch() throws JsonProcessingException {
+        try {
+            List<CodeList> body = codeListService.getDetailedCodesListForSearch(true);
+            return ResponseEntity.status(HttpStatus.OK).body(body);
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.CREATE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a code list")
+    public ResponseEntity<Object> createPartialCodeList(
+            @Parameter(description = "Code List", required = true) @RequestBody String body) {
+        try {
+            String id = codeListService.setCodesList(body, true);
+            return ResponseEntity.status(HttpStatus.OK).body(id);
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.UPDATE)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a code list")
+    public ResponseEntity<Object> updatePartialCodeList(
+            @PathVariable(Constants.ID) String componentId,
+            @Parameter(description = "Code List", required = true) @RequestBody String body) {
+        try {
+            String id = codeListService.setCodesList(componentId, body, true);
+            return ResponseEntity.status(HttpStatus.OK).body(id);
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+
+    @HasAccess(module = RBAC.Module.CODESLIST_PARTIALCODESLIST, privilege = RBAC.Privilege.DELETE)
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Delete a partial code list")
+    public ResponseEntity<Object> deletePartialCodeList(@PathVariable(Constants.ID) String notation) {
+        try {
+            codeListService.deleteCodeList(notation, true);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (RmesException e) {
+            return returnRmesException(e);
+        }
+    }
+}
---
+++ src/main/java/fr/insee/rmes/infrastructure/webservice/operations/SeriesResources.java
@@ -0,0 +1,163 @@
+package fr.insee.rmes.infrastructure.webservice.operations;
+
+import fr.insee.rmes.bauhaus_services.Constants;
+import fr.insee.rmes.bauhaus_services.OperationsDocumentationsService;
+import fr.insee.rmes.bauhaus_services.OperationsService;
+import fr.insee.rmes.config.swagger.model.IdLabelAltLabel;
+import fr.insee.rmes.config.swagger.model.IdLabelAltLabelSims;
+import fr.insee.rmes.exceptions.RmesException;
+import fr.insee.rmes.model.operations.Operation;
+import fr.insee.rmes.model.operations.PartialOperationSeries;
+import fr.insee.rmes.model.operations.Series;
+import fr.insee.rmes.rbac.HasAccess;
+import fr.insee.rmes.rbac.RBAC;
+import fr.insee.rmes.utils.XMLUtils;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Qualifier("Series")
+@RestController
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping("/operations")
+@ConditionalOnExpression("'${fr.insee.rmes.bauhaus.activeModules}'.contains('operations')")
+public class SeriesResources  {
+
+	protected final OperationsService operationsService;
+
+	protected final OperationsDocumentationsService documentationsService;
+
+	public SeriesResources(OperationsService operationsService, OperationsDocumentationsService documentationsService) {
+		this.operationsService = operationsService;
+		this.documentationsService = documentationsService;
+	}
+
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "List of series", responses = {@ApiResponse(content=@Content(schema=@Schema(type="array",implementation=IdLabelAltLabel.class)))})
+	public List<PartialOperationSeries> getSeries() throws RmesException {
+		return operationsService.getSeries();
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/withSims", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "List of series with related sims", responses = {@ApiResponse(content=@Content(schema=@Schema(type="array",implementation=IdLabelAltLabelSims.class)))})
+	public ResponseEntity<Object> getSeriesWIthSims() throws RmesException {
+		String series = operationsService.getSeriesWithSims();
+		return ResponseEntity.status(HttpStatus.OK).body(series);
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@io.swagger.v3.oas.annotations.Operation(
+	summary = "Get a series", responses = { @ApiResponse(content = @Content(schema = @Schema(implementation = Series.class)))})
+	public ResponseEntity<Object> getSeriesByID(@PathVariable(Constants.ID) String id,
+			@Parameter(hidden = true) @RequestHeader(required=false) String accept) throws RmesException {
+		if (accept != null && accept.equals(MediaType.APPLICATION_XML_VALUE)) {
+			return ResponseEntity.status(HttpStatus.OK).body(XMLUtils.produceXMLResponse(operationsService.getSeriesByID(id)));
+		} else {
+			return ResponseEntity.status(HttpStatus.OK).body(operationsService.getSeriesJsonByID(id));
+		}
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/advanced-search", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Series", responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = Series.class)))})
+	public ResponseEntity<Object> getSeriesForSearch() throws RmesException {
+		String series = operationsService.getSeriesForSearch();
+		return ResponseEntity.status(HttpStatus.OK).body(series);
+	}
+	
+	/**
+	 * Get series where stamp is the creator
+	 * If only id, label, altlabel are needed, prefere /series/seriesWithStamp/{stamp}
+	 */
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/advanced-search/{stamp}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Series", responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = Series.class)))})
+	public ResponseEntity<Object> getSeriesForSearchWithStamps(@Parameter(
+			description = "Timbre d'un utilisateur (format : ([A-Za-z0-9_-]+))",
+			required = true,
+			schema = @Schema(pattern = "([A-Za-z0-9_-]+)", type = "string")) @PathVariable(Constants.STAMP) String stamp
+			) throws RmesException {
+		String series = operationsService.getSeriesForSearchWithStamp(stamp);
+		return ResponseEntity.status(HttpStatus.OK).body(series);
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.UPDATE)
+	@PutMapping(value = "/series/{id}",
+		consumes = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Update a series")
+	public ResponseEntity<Object> setSeriesById(
+			@PathVariable(Constants.ID) String id,
+			@Parameter(description = "Series to update", required = true,
+			content = @Content(schema = @Schema(implementation = Series.class))) @RequestBody String body) throws RmesException {
+		operationsService.setSeries(id, body);
+		return ResponseEntity.ok(id);
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/{id}/operationsWithoutReport", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Operations without metadataReport",  responses = {@ApiResponse(content=@Content(schema=@Schema(type="array",implementation=Operation.class)))})
+	public ResponseEntity<Object> getOperationsWithoutReport(@PathVariable(Constants.ID) String id) throws RmesException {
+		String operations  = operationsService.getOperationsWithoutReport(id);
+		return ResponseEntity.status(HttpStatus.OK).body(operations);
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@GetMapping(value = "/series/{id}/operationsWithReport", produces = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Operations with metadataReport",  responses = {@ApiResponse(content=@Content(schema=@Schema(type="array",implementation=Operation.class)))})
+	public ResponseEntity<Object> getOperationsWithReport(@PathVariable(Constants.ID) String id) throws RmesException {
+		String operations  = operationsService.getOperationsWithReport(id);
+		return ResponseEntity.status(HttpStatus.OK).body(operations);
+	}
+
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.CREATE)
+	@PostMapping(value = "/series",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Create series")
+	public ResponseEntity<Object> createSeries(
+			@Parameter(description = "Series to create", required = true, 
+			content = @Content(schema = @Schema(implementation = Series.class))) @RequestBody String body) throws RmesException {
+		String id = operationsService.createSeries(body);
+		return ResponseEntity.status(HttpStatus.OK).body(id);
+	}
+
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.PUBLISH)
+	@PutMapping(value = "/series/{id}/validate",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Series validation")
+	public ResponseEntity<Object> setSeriesValidation(
+			@PathVariable(Constants.ID) String id) throws RmesException {
+		operationsService.setSeriesValidation(id);
+		return ResponseEntity.status(HttpStatus.OK).body(id);
+	}
+
+
+
+	@GetMapping(value = "/series/seriesWithStamp/{stamp}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@HasAccess(module = RBAC.Module.OPERATION_SERIES, privilege = RBAC.Privilege.READ)
+	@io.swagger.v3.oas.annotations.Operation(summary = "Series with given stamp as creator")
+	public ResponseEntity<Object> getSeriesWithStamp(@Parameter(
+			description = "Timbre d'un utilisateur (format : ([A-Za-z0-9_-]+))",
+			required = true,
+			schema = @Schema(pattern = "([A-Za-z0-9_-]+)", type = "string")) @PathVariable(Constants.STAMP) String stamp
+			) throws RmesException {
+		String series = operationsService.getSeriesWithStamp(stamp);
+		return ResponseEntity.status(HttpStatus.OK).body(series);
+	}
+	
+}
