44cf9b6adb45020e321365c1e2104e1b321a45ce
Emmanuel <demey.emmanuel@gmail.com>
2025-08-13 14:39:09 +0100

    feat: load component

--- src/main/java/fr/insee/rmes/infrastructure/graphql/Structure.java
+++ src/main/java/fr/insee/rmes/infrastructure/graphql/Structure.java
@@ -3,6 +3,29 @@ package fr.insee.rmes.infrastructure.graphql;
 import java.time.LocalDateTime;
 import java.util.List;
 
+record Component(
+        LocalDateTime created,
+        String identifiant,
+        String type,
+        List<String> contributor,
+        String labelLg2,
+        String labelLg1,
+        LocalDateTime modified,
+        String id,
+        String disseminationStatus,
+        String validationState
+) {}
+
+record ComponentDefinition(
+        Component component,
+        List<String> attachment,
+        LocalDateTime created,
+        LocalDateTime modified,
+        String id,
+        boolean required,
+        String order
+) {}
+
 public record Structure(
         String id,
         String identifiant,
@@ -13,6 +36,6 @@ public record Structure(
         LocalDateTime created,
         LocalDateTime modified,
         String disseminationStatus,
-        List<String> componentDefinitions,
+        List<ComponentDefinition> componentDefinitions,
         String validationState
 ) {}
