b4f88720f82684d1b51f00c1481cba515403ac57
Emmanuel <demey.emmanuel@gmail.com>
2025-10-06 12:17:25 +0100

    fix: remove unused import

--- module-bauhaus-bo/src/main/java/fr/insee/rmes/onion/application/OperationsConfiguration.java
+++ module-bauhaus-bo/src/main/java/fr/insee/rmes/onion/application/OperationsConfiguration.java
@@ -1,6 +1,5 @@
 package fr.insee.rmes.onion.application;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.rmes.domain.port.clientside.DDIService;
 import fr.insee.rmes.domain.port.clientside.FamilyService;
 import fr.insee.rmes.domain.port.serverside.DDIRepository;
@@ -12,7 +11,6 @@ import fr.insee.rmes.onion.domain.port.serverside.DocumentationRepository;
 import fr.insee.rmes.onion.domain.services.operations.DocumentationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OperationsConfiguration {
