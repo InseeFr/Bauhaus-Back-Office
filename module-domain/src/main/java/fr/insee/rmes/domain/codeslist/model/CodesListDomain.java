c2b7629170b1ae41e38260bf012544e0740bf02b
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2025-10-06 09:22:50 +0100

    feat: add new field for codeslist with the new ORM (#1017)
    * feat: add new field for codeslist with the new ORM
    
    * fix: solve issue with config
    
    * feat: add contributors

--- module-domain/src/main/java/fr/insee/rmes/domain/codeslist/model/CodesListDomain.java
+++ module-domain/src/main/java/fr/insee/rmes/domain/codeslist/model/CodesListDomain.java
@@ -11,12 +11,45 @@ public class CodesListDomain {
     private final String labelLg1;
     private final String labelLg2;
     private final String range;
-    
-    public CodesListDomain(String id, String uri, String labelLg1, String labelLg2, String range) {
-        this.id = id;
+    private final String descriptionLg1;
+    private final String descriptionLg2;
+    private final String lastCodeUriSegment;
+    private final String created;
+    private final String creator;
+    private final String validationState;
+    private final String disseminationStatus;
+    private final String modified;
+    private final String iriParent;
+
+    public CodesListDomain(
+            String uri,
+            String id,
+            String labelLg1,
+            String labelLg2,
+            String descriptionLg1,
+            String descriptionLg2,
+            String range,
+            String lastCodeUriSegment,
+            String created,
+            String creator,
+            String validationState,
+            String disseminationStatus,
+            String modified,
+            String iriParent
+            ) {
         this.uri = uri;
+        this.id = id;
         this.labelLg1 = labelLg1;
         this.labelLg2 = labelLg2;
+        this.descriptionLg1 = descriptionLg1;
+        this.descriptionLg2 = descriptionLg2;
+        this.lastCodeUriSegment = lastCodeUriSegment;
+        this.created = created;
+        this.creator = creator;
+        this.validationState = validationState;
+        this.disseminationStatus = disseminationStatus;
+        this.modified = modified;
+        this.iriParent = iriParent;
         this.range = range;
     }
     
@@ -39,7 +72,43 @@ public class CodesListDomain {
     public String getRange() {
         return range;
     }
-    
+
+    public String getDescriptionLg1() {
+        return descriptionLg1;
+    }
+
+    public String getDescriptionLg2() {
+        return descriptionLg2;
+    }
+
+    public String getLastCodeUriSegment() {
+        return lastCodeUriSegment;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public String getValidationState() {
+        return validationState;
+    }
+
+    public String getDisseminationStatus() {
+        return disseminationStatus;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public String getIriParent() {
+        return iriParent;
+    }
+
     @Override
     public String toString() {
         return "CodesListDomain{" +
