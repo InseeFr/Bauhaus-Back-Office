388404144401a80321cafa9726390d06da2f1222
Emmanuel <demey.emmanuel@gmail.com>
2025-10-06 12:04:56 +0100

    feat: be able to update a Physical Instance

--- module-bauhaus-bo/src/main/java/fr/insee/rmes/onion/application/OperationsConfiguration.java
+++ module-bauhaus-bo/src/main/java/fr/insee/rmes/onion/application/OperationsConfiguration.java
@@ -1,5 +1,6 @@
 package fr.insee.rmes.onion.application;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.rmes.domain.port.clientside.DDIService;
 import fr.insee.rmes.domain.port.clientside.FamilyService;
 import fr.insee.rmes.domain.port.serverside.DDIRepository;
@@ -11,6 +12,7 @@ import fr.insee.rmes.onion.domain.port.serverside.DocumentationRepository;
 import fr.insee.rmes.onion.domain.services.operations.DocumentationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OperationsConfiguration {
