fb843ca011782c5bbd2ac3d82624a5c43ea789dc
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2025-10-22 12:27:57 +0100

    feat: add ddi3 to ddi4 converters (#1025)
    * fix: disable test compilation for docker build
    
    * feat: improve CI
    
    * fix: solve unit test
    
    * fix: unit tests
    
    * fix: unit tests
    
    * feat: add ddi3 to ddi4 converters
    
    * fix: solve unit test
    
    * fix: solve sonar issues

--- module-domain/src/test/java/fr/insee/rmes/ArchUnitTest.java
+++ module-domain/src/test/java/fr/insee/rmes/ArchUnitTest.java
@@ -22,7 +22,8 @@ class ArchUnitTest {
                 "fr.insee.rmes.domain..",
                 "java..",
                 "org.json..",
-                "com.fasterxml.jackson.."
+                "com.fasterxml.jackson..",
+                "jakarta.xml.."
             );
 
         rule.check(importedClasses);
@@ -35,7 +36,11 @@ class ArchUnitTest {
             .should().onlyDependOnClassesThat().resideInAnyPackage(
                 "fr.insee.rmes.domain..",
                 "java..",
-                "org.slf4j.."
+                "org.slf4j..",
+                "javax.xml..",
+                "org.w3c.dom..",
+                "jakarta.xml..",
+                "org.xml.sax.."
             );
 
         rule.check(importedClasses);
