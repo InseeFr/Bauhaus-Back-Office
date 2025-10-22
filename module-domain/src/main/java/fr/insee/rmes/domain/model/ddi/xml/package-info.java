ce4569baac8891b256af53415a0f491ca3614719
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2025-10-22 12:27:57 +0100

    feat: add ddi3 to ddi4 converters (#1025)
    * fix: disable test compilation for docker build
    
    * feat: improve CI
    
    * fix: solve unit test
    
    * fix: unit tests
    
    * fix: unit tests
    
    * feat: add ddi3 to ddi4 converters
    
    * fix: solve unit test
    
    * fix: solve sonar issues

---
+++ module-domain/src/main/java/fr/insee/rmes/domain/model/ddi/xml/package-info.java
@@ -0,0 +1,15 @@
+@XmlSchema(
+    namespace = "ddi:instance:3_3",
+    xmlns = {
+        @XmlNs(prefix = "", namespaceURI = "ddi:instance:3_3"),
+        @XmlNs(prefix = "r", namespaceURI = "ddi:reusable:3_3"),
+        @XmlNs(prefix = "pi", namespaceURI = "ddi:physicalinstance:3_3"),
+        @XmlNs(prefix = "lp", namespaceURI = "ddi:logicalproduct:3_3")
+    },
+    elementFormDefault = XmlNsForm.QUALIFIED
+)
+package fr.insee.rmes.domain.model.ddi.xml;
+
+import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlNsForm;
+import jakarta.xml.bind.annotation.XmlSchema;
