14e25b58d3f9edd77ee13313291c90ff6a78525e
Emmanuel <demey.emmanuel@gmail.com>
2025-10-28 11:46:48 +0000

    refactor: hardcode GraphDB Docker image version in testcontainers

--- module-testcontainer-graphdb/src/main/java/fr/insee/rmes/testcontainers/WithGraphDBContainer.java
+++ module-testcontainer-graphdb/src/main/java/fr/insee/rmes/testcontainers/WithGraphDBContainer.java
@@ -1,47 +1,15 @@
 package fr.insee.rmes.testcontainers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import fr.insee.rmes.graphdb.RdfConnectionDetails;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 @Testcontainers
 public class WithGraphDBContainer {
 
     @Container
-    public static final GraphDBContainer container = new GraphDBContainer(getDockerImageName());
-
-    private static String getDockerImageName() {
-        Path filePath = Paths.get(System.getProperty("user.dir"), "compose.yaml");
-        File file = filePath.toFile();
-
-        String serviceName = "graphdb";
-
-        YAMLMapper yamlMapper = new YAMLMapper();
-        JsonNode rootNode;
-        try {
-            rootNode = yamlMapper.readTree(file);
-            JsonNode servicesNode = rootNode.get("services");
-            if (servicesNode != null && servicesNode.has(serviceName)) {
-                JsonNode serviceNode = servicesNode.get(serviceName);
-                JsonNode imageNode = serviceNode.get("image");
-                if (imageNode != null) {
-                    return imageNode.asText();
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return "";
-    }
+    public static final GraphDBContainer container = new GraphDBContainer("ontotext/graphdb:10.8.4");
 
 
     protected static RdfConnectionDetails getRdfGestionConnectionDetails() {
