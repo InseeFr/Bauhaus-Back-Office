ca46d4f3122081c7d44e7a93e275a887280d1652
Fabrice B <fabrice.bibonne@insee.fr>
2025-11-21 15:46:27 +0100

    fix (collections label) label naming

--- module-bauhaus-bo/src/main/java/fr/insee/rmes/modules/concepts/collections/webservice/CollectionResponse.java
+++ module-bauhaus-bo/src/main/java/fr/insee/rmes/modules/concepts/collections/webservice/CollectionResponse.java
@@ -1,8 +1,7 @@
 package fr.insee.rmes.modules.concepts.collections.webservice;
 
-import fr.insee.rmes.modules.concepts.collections.domain.model.Collection;
-import fr.insee.rmes.modules.commons.domain.model.Lang;
 import fr.insee.rmes.modules.commons.domain.model.LocalisedLabel;
+import fr.insee.rmes.modules.concepts.collections.domain.model.Collection;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -10,7 +9,7 @@ import java.util.List;
 
 public record CollectionResponse(
         String id,
-        List<LocalisedLabelResponse> prefLabels,
+        List<LocalisedLabelResponse> labels,
         LocalDateTime created,
         LocalDateTime modified,
         List<LocalisedLabelResponse> descriptions,
