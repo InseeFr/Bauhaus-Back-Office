925cb0bb9201a1704f2fabe80eb3d3f2423284f9
Nicolas Echallier <rkouere@users.noreply.github.com>
2026-04-13 09:19:27 +0200

    change rbac

--- module-domain/src/main/java/fr/insee/rmes/domain/Roles.java
+++ module-domain/src/main/java/fr/insee/rmes/domain/Roles.java
@@ -2,6 +2,6 @@ package fr.insee.rmes.domain;
 
 public class Roles {
 
-	public static final String ADMIN = "Administrateur_RMESGNCS";
+	public static final String ADMIN = "bauhaus_administrateur_RMES";
 
 }
