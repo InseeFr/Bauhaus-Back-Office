feb469c424d6c847c47c51e9f60d6628d78f38ff
Emmanuel Demey <demey.emmanuel@gmail.com>
2026-05-09 12:53:09 +0100

    feat: review module architecture

---
+++ module-utility/src/test/java/fr/insee/rmes/utility/archunit/UtilityNoDownstreamDependencyTest.java
@@ -0,0 +1,37 @@
+package fr.insee.rmes.utility.archunit;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(packages = "fr.insee.rmes")
+class UtilityNoDownstreamDependencyTest {
+
+    @ArchTest
+    static final ArchRule no_dependency_on_operation_ddi_or_bauhaus_bo =
+            noClasses().should().dependOnClassesThat().resideInAnyPackage(
+                    "fr.insee.rmes.modules.operation..",
+                    "fr.insee.rmes.modules.ddi..",
+                    "fr.insee.rmes.persistance..",
+                    "fr.insee.rmes.bauhaus_services..",
+                    "fr.insee.rmes.onion..",
+                    "fr.insee.rmes.model..",
+                    "fr.insee.rmes.exceptions..",
+                    "fr.insee.rmes.utils..",
+                    "fr.insee.rmes.modules.datasets..",
+                    "fr.insee.rmes.modules.clientconfig..",
+                    "fr.insee.rmes.modules.users..",
+                    "fr.insee.rmes.modules.structures..",
+                    "fr.insee.rmes.modules.operations..",
+                    "fr.insee.rmes.modules.codeslists..",
+                    "fr.insee.rmes.modules.checks..",
+                    "fr.insee.rmes.modules.commons..",
+                    "fr.insee.rmes.modules.classifications..",
+                    "fr.insee.rmes.modules.organisations..",
+                    "fr.insee.rmes.modules.geographies..",
+                    "fr.insee.rmes.modules.shared_kernel..",
+                    "fr.insee.rmes.modules.concepts.."
+            );
+}
