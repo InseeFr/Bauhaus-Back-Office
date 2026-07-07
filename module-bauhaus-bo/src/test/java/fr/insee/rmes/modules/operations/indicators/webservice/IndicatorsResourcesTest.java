6f2fda6ca3d24f7e02d6a4bb121acd6074221cb6
GtanSndil <mail@ad.insee.intra>
2026-07-07 17:50:05 +0200

    pom: overload bouncycastle (from minio)
    and upgrade other dependencies

--- module-bauhaus-bo/src/test/java/fr/insee/rmes/modules/operations/indicators/webservice/IndicatorsResourcesTest.java
+++ module-bauhaus-bo/src/test/java/fr/insee/rmes/modules/operations/indicators/webservice/IndicatorsResourcesTest.java
@@ -12,7 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.security.oauth2.server.resource.autoconfigure.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
